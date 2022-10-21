72076f849f5b8e42c3c1899c34d7070f1c764386
Frederic Wong <frederic.wong@simplybusiness.co.uk>
2022-10-21 14:15:58 +0100

    Allow rover to have an intial starting point

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,3 +1,8 @@
 class MarsRover
-  
+  attr_accessor :starting_coordinate_x, :starting_coordinate_y
+
+  def initialize(starting_coordinate_x:, starting_coordinate_y:)
+    @starting_coordinate_x = starting_coordinate_x
+    @starting_coordinate_y = starting_coordinate_y 
+  end
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../lib/mars_rover.rb'
 
 describe MarsRover do
+  it 'has an initial starting point' do
+    starting_coordinate_x = 0
+    starting_coordinate_y = 0
+    mars_rover = MarsRover.new(starting_coordinate_x: starting_coordinate_x, starting_coordinate_y: starting_coordinate_y)
+
+    expect(mars_rover.starting_coordinate_x).to eq 0
+    expect(mars_rover.starting_coordinate_y).to eq 0
+  end
 end
 
