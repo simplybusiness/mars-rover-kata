08dbfe69e86ebe035fb524caf0466c2c6fd8ce35
Frederic Wong <frederic.wong@simplybusiness.co.uk>
2022-10-21 15:08:06 +0100

    Refactor test by remove duplication

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,14 +1,12 @@
 require_relative '../lib/mars_rover.rb'
 
 describe MarsRover do
+  let(:starting_coordinate_x) { 0 }
+  let(:starting_coordinate_y) { 0 }
+  let(:starting_direction) { 'N' }
+
   context 'when give an initial position' do
     it 'has an initial starting point and direction' do
-      starting_coordinate_x = 0
-      starting_coordinate_y = 0
-      starting_direction = 'N'
-
-      mars_rover = MarsRover.new(starting_coordinate_x: starting_coordinate_x, starting_coordinate_y: starting_coordinate_y, starting_direction: starting_direction)
-
       expect(mars_rover.starting_coordinate_x).to eq 0
       expect(mars_rover.starting_coordinate_y).to eq 0
       expect(mars_rover.starting_direction).to eq 'N'
@@ -17,15 +15,13 @@ describe MarsRover do
 
   context 'when given a set of commands' do
     it 'has an initial set of commands' do
-      starting_coordinate_x = 0
-      starting_coordinate_y = 0
-      starting_direction = 'N'
-      commands = []
-
-      mars_rover = MarsRover.new(starting_coordinate_x: starting_coordinate_x, starting_coordinate_y: starting_coordinate_y, starting_direction: starting_direction, commands: commands)
-
       expect(mars_rover.commands).to eq []
     end
   end
 end
+
+def mars_rover
+  MarsRover.new(starting_coordinate_x: starting_coordinate_x, starting_coordinate_y: starting_coordinate_y, starting_direction: starting_direction)
+end
+
 
