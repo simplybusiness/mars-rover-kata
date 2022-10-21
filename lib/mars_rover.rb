93a0898ebeb942399093c90987a61a6f665f0a99
Frederic Wong <frederic.wong@simplybusiness.co.uk>
2022-10-21 14:19:33 +0100

    Add starting direction

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,8 +1,9 @@
 class MarsRover
-  attr_accessor :starting_coordinate_x, :starting_coordinate_y
+  attr_accessor :starting_coordinate_x, :starting_coordinate_y, :starting_direction
 
-  def initialize(starting_coordinate_x:, starting_coordinate_y:)
+  def initialize(starting_coordinate_x:, starting_coordinate_y:, starting_direction:)
     @starting_coordinate_x = starting_coordinate_x
     @starting_coordinate_y = starting_coordinate_y 
+    @starting_direction = starting_direction
   end
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,13 +1,27 @@
 require_relative '../lib/mars_rover.rb'
 
 describe MarsRover do
-  it 'has an initial starting point' do
-    starting_coordinate_x = 0
-    starting_coordinate_y = 0
-    mars_rover = MarsRover.new(starting_coordinate_x: starting_coordinate_x, starting_coordinate_y: starting_coordinate_y)
+  describe 'Initial position' do
+    it 'has an initial starting point' do
+      starting_coordinate_x = 0
+      starting_coordinate_y = 0
+      starting_direction = 'N'
 
-    expect(mars_rover.starting_coordinate_x).to eq 0
-    expect(mars_rover.starting_coordinate_y).to eq 0
+      mars_rover = MarsRover.new(starting_coordinate_x: starting_coordinate_x, starting_coordinate_y: starting_coordinate_y, starting_direction: starting_direction)
+
+      expect(mars_rover.starting_coordinate_x).to eq 0
+      expect(mars_rover.starting_coordinate_y).to eq 0
+    end
+
+    it 'has an intial starting direction' do
+      starting_coordinate_x = 0
+      starting_coordinate_y = 0
+      starting_direction = 'N'
+
+      mars_rover = MarsRover.new(starting_coordinate_x: starting_coordinate_x, starting_coordinate_y: starting_coordinate_y, starting_direction: starting_direction)
+
+      expect(mars_rover.starting_direction).to eq 'N'
+    end
   end
 end
 
