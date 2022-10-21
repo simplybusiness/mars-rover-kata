023caabee7064355ec60aa0edcd6379686b3be8f
Frederic Wong <frederic.wong@simplybusiness.co.uk>
2022-10-21 15:04:07 +0100

    Allow rover to have commands

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,9 +1,10 @@
 class MarsRover
-  attr_accessor :starting_coordinate_x, :starting_coordinate_y, :starting_direction
+  attr_accessor :starting_coordinate_x, :starting_coordinate_y, :starting_direction, :commands
 
-  def initialize(starting_coordinate_x:, starting_coordinate_y:, starting_direction:)
+  def initialize(starting_coordinate_x:, starting_coordinate_y:, starting_direction:, commands: [])
     @starting_coordinate_x = starting_coordinate_x
     @starting_coordinate_y = starting_coordinate_y 
     @starting_direction = starting_direction
+    @commands = commands
   end
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/mars_rover.rb'
 
 describe MarsRover do
-  describe 'Initial position' do
+  context 'when give an initial position' do
     it 'has an initial starting point and direction' do
       starting_coordinate_x = 0
       starting_coordinate_y = 0
@@ -12,6 +12,19 @@ describe MarsRover do
       expect(mars_rover.starting_coordinate_x).to eq 0
       expect(mars_rover.starting_coordinate_y).to eq 0
       expect(mars_rover.starting_direction).to eq 'N'
+    end
+  end
+
+  context 'when given a set of commands' do
+    it 'has an initial set of commands' do
+      starting_coordinate_x = 0
+      starting_coordinate_y = 0
+      starting_direction = 'N'
+      commands = []
+
+      mars_rover = MarsRover.new(starting_coordinate_x: starting_coordinate_x, starting_coordinate_y: starting_coordinate_y, starting_direction: starting_direction, commands: commands)
+
+      expect(mars_rover.commands).to eq []
     end
   end
 end
