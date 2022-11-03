4679ce5013e299fa7b07e4dd0264dbb643a2fec2
Frederic Wong <frederic.wong@simplybusiness.co.uk>
2022-11-03 11:39:05 +0000

    Remove old code

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -26,32 +26,4 @@ class MarsRover
 
     @coordinate
   end
-
-  private
-
-  def move_forward
-    case @compass.direction
-    when 'N'
-      @coordinate.up
-    when 'E'
-      @coordinate.right
-    when 'S'
-      @coordinate.down
-    when 'W'
-      @coordinate.left
-    end
-  end
-
-  def move_backward
-    case @compass.direction
-    when 'N'
-      @coordinate.down
-    when 'E'
-      @coordinate.left
-    when 'S'
-      @coordinate.up
-    when 'W'
-      @coordinate.right
-    end
-  end
 end
