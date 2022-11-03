3bfbb51fa39367a9a099501d893d7f6d7458b03c
Frederic Wong <frederic.wong@simplybusiness.co.uk>
2022-11-03 11:30:58 +0000

    Add new CurrentPosition class

---
+++ lib/current_postion.rb
@@ -0,0 +1,32 @@
+class CurrentPosition
+  def initialize (compass, coordinate)
+    @compass = compass
+    @coordinate = coordinate
+  end
+
+  def move_forward
+    case @compass.direction
+    when 'N'
+      @coordinate.up
+    when 'E'
+      @coordinate.right
+    when 'S'
+      @coordinate.down
+    when 'W'
+      @coordinate.left
+    end
+  end
+
+  def move_backward
+    case @compass.direction
+    when 'N'
+      @coordinate.down
+    when 'E'
+      @coordinate.left
+    when 'S'
+      @coordinate.up
+    when 'W'
+      @coordinate.right
+    end
+  end
+end
