cb43f30f77ac09611ed78fb7f82321004e386cc0
SaiyuktaR <102798914+SaiyuktaR@users.noreply.github.com>
2023-01-18 12:30:42 +0000

    red: Rover initlialize variable test

--- spec/lib/rover_spec.rb
+++ spec/lib/rover_spec.rb
@@ -1,5 +1,13 @@
 require "rover"
 
 RSpec.describe Rover do
+  describe '#initialize' do
+    it "receives a starting point and direction" do
+      rover = Rover.new(x: 10, y: 20, direction: "N")
+      expect(rover.x).to eq 10
+      expect(rover.y).to eq 20
+      expect(rover.direction).to eq "N"
+    end
+  end
 
 end
