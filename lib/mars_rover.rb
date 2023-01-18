a551e15ac3ae3e12424f39e00c924e3bc9b1cb6b
Frederic Wong <frederic.wong@simplybusiness.co.uk>
2023-01-18 12:10:13 +0000

    Add position and direction to mars rover

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,4 +1,8 @@
 class MarsRover
-  def initialize
+  attr_reader :position, :direction
+
+  def initialize(x:, y:, facing_direction:)
+    @position = Position.new(x: x, y: y)
+    @direction = Direction.new(facing_direction: facing_direction)
   end
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,7 +1,20 @@
 require_relative 'spec_helper'
 
 describe MarsRover do
-  it 'says hello' do
-    expect(described_class.new.hello).to eq "hello"
+  it 'has a starting position' do
+    x = 0
+    y = 0
+    facing_direction = 'N'
+
+    expect(described_class.new(x: x, y: y, facing_direction: facing_direction).position.x).to eq x
+    expect(described_class.new(x: x, y: y, facing_direction: facing_direction).position.y).to eq y
+  end
+
+  it 'has a starting direction' do
+    x = 0
+    y = 0
+    facing_direction = 'N'
+
+    expect(described_class.new(x: x, y: y, facing_direction: facing_direction).direction.facing_direction).to eq facing_direction
   end
 end
