c17d1607f116a3d2bca30bf14012f5c80f77a6de
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2023-01-18 12:18:26 +0000

    refactor: reduce cognitive complexity of init of MarsRover

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,8 +2,10 @@ require './lib/mars_rover'
 
 RSpec.describe MarsRover do
   it 'knows its initial starting coordinates of x and y' do
-    expect(MarsRover.new(x:200, y:0).x).to eq 200
-    expect(MarsRover.new(x:0, y:400).y).to eq 400
+    mars_rover = MarsRover.new(x:200, y:400)
+
+    expect(mars_rover.x).to eq 200
+    expect(mars_rover.y).to eq 400
   end
 
   it 'knows its compass direction'
