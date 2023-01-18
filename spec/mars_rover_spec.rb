1525ca068216e55977ed2a93c20ebd3d14ae524c
Jessica Findley <jessica.findley@simplybusiness.co.uk>
2023-01-18 13:30:59 +0000

    adjusted typos in test

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,14 +2,14 @@ require './lib/mars_rover'
 
 RSpec.describe MarsRover do
   it 'knows its initial starting coordinates of x and y' do
-    mars_rover = MarsRover.new(x: 200, y: 400)
+    mars_rover = MarsRover.new(x: 200, y: 400, direction: "S")
 
     expect(mars_rover.coordinates).to eq [200, 400]
   end
 
   it 'knows its compass direction' do
-    mars_rover = MarsRover.new(x: 200, y: 400, direction: "S")
+    mars_rover = MarsRover.new(x: 200, y: 400, direction: "N")
 
-    expect(mars_rover.direction).to eq "S"
+    expect(mars_rover.direction).to eq "N"
   end
 end
