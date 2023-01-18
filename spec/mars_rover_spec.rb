a690b40340329a98cc8c0754cb77b125936757a3
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2023-01-18 12:59:34 +0000

    refactor: inline x and y coordinates

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,11 +2,9 @@ require './lib/mars_rover'
 
 RSpec.describe MarsRover do
   it 'knows its initial starting coordinates of x and y' do
-    x_coordinate = 200
-    y_coordinate = 400
-    mars_rover = MarsRover.new(x: x_coordinate, y: y_coordinate)
+    mars_rover = MarsRover.new(x: 200, y: 400)
 
-    expect(mars_rover.coordinates).to eq [x_coordinate, y_coordinate]
+    expect(mars_rover.coordinates).to eq [200, 400]
   end
 
   it 'knows its compass direction'
