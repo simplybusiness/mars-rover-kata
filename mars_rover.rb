ab12536a7a104ed56b47ff82af57dbb8b470b777
m-a-l <marie-alix.lesellier@simplybusiness.co.uk>
2023-01-20 15:07:52 +0000

    start simpler

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,33 +1,32 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-# Mars ROver
-class MarsRover
-  attr_accessor :x, :y, :orientation
+# # Mars ROver
+# class MarsRover
+#   attr_accessor :x, :y, :orientation
 
-  def initialize(x,y,orientation)
-    @x = x
-    @y = y
-    @orientation = "N"
-  end
+#   def initialize(x,y,orientation)
+#     @x = x
+#     @y = y
+#     @orientation = "N"
+#   end
 
-  def execute(commands)
-    process(commands).each do |command|
-      if command == "f"
-        @y = @y + 1
-      elsif command == "b"
-        @y = @y - 1
-      elsif command == "r"
-        @orientation = "E"
-      elsif command == "l"
-        @orientation = "W"
-      else
-        @y
-      end
-    end
-  end
+#   def execute(commands)
+#     process(commands).each do |command|
+#       if command == "f"
+#         @y = @y + 1
+#       elsif command == "b"
+#         @y = @y - 1
+#       elsif command == "r"
+#         @orientation = "E"
+#       elsif command == "l"
+#         @orientation = "W"
+#       else
+#         @y
+#       end
+#     end
+#   end
 
-  def process(commands)
-    commands.chars
-  end
-end
-
+#   def process(commands)
+#     commands.chars
+#   end
+# end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,55 +1,102 @@
 require 'rspec'
-require_relative '../mars_rover'
+# require_relative '../mars_rover'
+class MarsRover
+  attr_accessor :x, :y, :direction
+
+  def initialize(x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
+  end
+
+  def execute(command)
+    true
+  end
+end
 
 RSpec.describe MarsRover do
-  let(:rover) {MarsRover.new(2,0,"N")}
+  let(:rover) {MarsRover.new(0,0, 'N')}
   let(:commands) {"fff"}
   let(:complex_command) {"flfrb"}
-  describe 'x' do
-    it 'returns x' do
-      expect(rover.x).to eq(2)
-    end
+
+  it 'knows its position on the planet' do
+    mars_rover = MarsRover.new(0, 0, 'N')
+    expect(mars_rover.x).to eq(0)
+    expect(mars_rover.y).to eq(0)
   end
-  describe 'y' do
-    it 'returns y' do
-      expect(rover.y).to eq(0)
-    end
+
+  it 'knows its position when at 1,1 on the planet' do
+    mars_rover = MarsRover.new(1, 1, 'N')
+    expect(mars_rover.x).to eq(1)
+    expect(mars_rover.y).to eq(1)
   end
-  describe 'orientation' do
-    it 'returns North' do
-      expect(rover.orientation).to eq("N")
-    end
+
+  it 'knows the direction its facing' do
+    mars_rover = MarsRover.new(0, 0, 'N')
+    expect(mars_rover.direction).to eq('N')
   end
-  describe 'execute' do 
-    it 'moves the rover forward by one space' do
-      rover.execute("f")
-      expect(rover.y).to eq(1)
-    end
-    it 'moves the rover backwards by one space' do
-      rover.execute("b")
-      expect(rover.y).to eq(-1)
-    end
-    it 'process commands' do
-      series_of_commands = rover.process(commands)
-      expect(series_of_commands).to eq(["f","f","f"])
-    end
-    it 'moves the rover forward by three steps' do
-      rover.execute(commands)
-      expect(rover.y).to eq(3)
-    end
-    it 'rotates right' do
-      rover.execute('r')
-      expect(rover.orientation).to eq("E")
-    end
-    it 'rotates left' do
-      rover.execute('l')
-      expect(rover.orientation).to eq("W")
-    end
-    it 'can process both move;ent and rotation' do
-      rover.execute(complex_command)
-      expect(rover.y).to eq(1)
-      expect(rover.x).to eq(0)
-      expect(rover.orientation).to eq("N")
-    end
+
+  it 'knows the direction its facing when it lands facing south' do
+    mars_rover = MarsRover.new(0, 0, 'S')
+    expect(mars_rover.direction).to eq('S')
   end
+
+  it 'can receive an array of commands' do 
+    mars_rover = MarsRover.new(0, 0, 'S')
+    expect(mars_rover.execute(['f', 'b', 'f'])).to eq(true)
+  end
+
+  # it 'changes its position accordingly when receiving a forward command' do
+  #   mars_rover = MarsRover.new(0, 0, 'N')
+  #   expect(mars_rover.x).to eq(0)
+  #   expect(mars_rover.y).to eq(1)
+  # end
+
+  # describe 'x' do
+  #   it 'returns x' do
+  #     expect(rover.x).to eq(2)
+  #   end
+  # end
+  # describe 'y' do
+  #   it 'returns y' do
+  #     expect(rover.y).to eq(0)
+  #   end
+  # end
+  # describe 'orientation' do
+  #   it 'returns North' do
+  #     expect(rover.orientation).to eq("N")
+  #   end
+  # end
+  # describe 'execute' do 
+  #   it 'moves the rover forward by one space' do
+  #     rover.execute("f")
+  #     expect(rover.y).to eq(1)
+  #   end
+  #   it 'moves the rover backwards by one space' do
+  #     rover.execute("b")
+  #     expect(rover.y).to eq(-1)
+  #   end
+  #   it 'process commands' do
+  #     series_of_commands = rover.process(commands)
+  #     expect(series_of_commands).to eq(["f","f","f"])
+  #   end
+  #   it 'moves the rover forward by three steps' do
+  #     rover.execute(commands)
+  #     expect(rover.y).to eq(3)
+  #   end
+  #   it 'rotates right' do
+  #     rover.execute('r')
+  #     expect(rover.orientation).to eq("E")
+  #   end
+  #   it 'rotates left' do
+  #     rover.execute('l')
+  #     expect(rover.orientation).to eq("W")
+  #   end
+  #   it 'can process both move;ent and rotation' do
+  #     rover.execute(complex_command)
+  #     expect(rover.y).to eq(1)
+  #     expect(rover.x).to eq(0)
+  #     expect(rover.orientation).to eq("N")
+  #   end
+  # end
 end
