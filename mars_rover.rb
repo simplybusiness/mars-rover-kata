631124bc32d6a7e32e541d7673971276efdc949a
Sebastian Torrente <sebastian.torrente@simplybusiness.co.uk>
2023-01-20 16:26:18 +0000

    redoing movement on the y axis

--- mars_rover.rb
+++ mars_rover.rb
@@ -10,16 +10,44 @@ class MarsRover
     @orientation = "N"
   end
 
+  def rotates(orientation, direction)
+    wind_rose = ["N","E","S","W"]
+    facing = wind_rose.find_index(orientation)
+    if direction == "l"
+      @orientation = wind_rose[facing - 1]
+    elsif direction == "r"
+      @orientation = wind_rose[facing + 1]
+    else
+      @orientation
+    end
+  end
+
+  def move(direction)
+    if direction == "f"
+      +1
+    elsif direction == "b"
+      -1
+    else
+      1
+    end
+  end
+
   def execute(commands)
     process(commands).each do |command|
-      if command == "f"
-        @y = @y + 1
-      elsif command == "b"
-        @y = @y - 1
-      elsif command == "r"
-        @orientation = "E"
+      # binding.pry
+      # @orientation = rotates(@orientation, command) if commands == ("l" or "r") 
+      if command == "r"
+        @orientation = rotates(@orientation, "r")
       elsif command == "l"
-        @orientation = "W"
+        @orientation = rotates(@orientation, "l")
+      elsif @orientation == "N"
+        @y = @y + (1 * move(command))
+      elsif @orientation == "S"
+        @y = @y - (1 * move(command))
+      elsif @orientation == "E"
+        @x = @x + (1 * move(command))
+      elsif @orientation == "W"
+        @x = @x - (1 * move(command))
       else
         @y
       end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,54 +2,33 @@ require 'rspec'
 require_relative '../mars_rover'
 
 RSpec.describe MarsRover do
-  let(:rover) {MarsRover.new(2,0,"N")}
+  let(:rover) {MarsRover.new(0,0,"N")}
   let(:commands) {"fff"}
-  let(:complex_command) {"flfrb"}
-  describe 'x' do
+  let(:complex_command) {"flfrb"} # You end in (-1, 0, "N")
+
+  describe 'position' do
     it 'returns x' do
-      expect(rover.x).to eq(2)
+      expect(rover.x).to eq(0)
     end
-  end
-  describe 'y' do
+
     it 'returns y' do
       expect(rover.y).to eq(0)
     end
-  end
-  describe 'orientation' do
+
     it 'returns North' do
       expect(rover.orientation).to eq("N")
     end
   end
-  describe 'execute' do 
+
+  describe 'movement for a North facing Mars Rover' do
     it 'moves the rover forward by one space' do
       rover.execute("f")
       expect(rover.y).to eq(1)
     end
+
     it 'moves the rover backwards by one space' do
       rover.execute("b")
       expect(rover.y).to eq(-1)
-    end
-    it 'process commands' do
-      series_of_commands = rover.process(commands)
-      expect(series_of_commands).to eq(["f","f","f"])
-    end
-    it 'moves the rover forward by three steps' do
-      rover.execute(commands)
-      expect(rover.y).to eq(3)
-    end
-    it 'rotates right' do
-      rover.execute('r')
-      expect(rover.orientation).to eq("E")
-    end
-    it 'rotates left' do
-      rover.execute('l')
-      expect(rover.orientation).to eq("W")
-    end
-    it 'can process both move;ent and rotation' do
-      rover.execute(complex_command)
-      expect(rover.y).to eq(1)
-      expect(rover.x).to eq(0)
-      expect(rover.orientation).to eq("N")
     end
   end
 end
