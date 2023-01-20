57d137e4ba8a526a1775da8908a581f164c40703
Sebastian Torrente <sebastian.torrente@simplybusiness.co.uk>
2023-01-20 16:28:20 +0000

    simplified the code

--- mars_rover.rb
+++ mars_rover.rb
@@ -10,18 +10,6 @@ class MarsRover
     @orientation = "N"
   end
 
-  def rotates(orientation, direction)
-    wind_rose = ["N","E","S","W"]
-    facing = wind_rose.find_index(orientation)
-    if direction == "l"
-      @orientation = wind_rose[facing - 1]
-    elsif direction == "r"
-      @orientation = wind_rose[facing + 1]
-    else
-      @orientation
-    end
-  end
-
   def move(direction)
     if direction == "f"
       +1
@@ -34,23 +22,7 @@ class MarsRover
 
   def execute(commands)
     process(commands).each do |command|
-      # binding.pry
-      # @orientation = rotates(@orientation, command) if commands == ("l" or "r") 
-      if command == "r"
-        @orientation = rotates(@orientation, "r")
-      elsif command == "l"
-        @orientation = rotates(@orientation, "l")
-      elsif @orientation == "N"
-        @y = @y + (1 * move(command))
-      elsif @orientation == "S"
-        @y = @y - (1 * move(command))
-      elsif @orientation == "E"
-        @x = @x + (1 * move(command))
-      elsif @orientation == "W"
-        @x = @x - (1 * move(command))
-      else
-        @y
-      end
+      @y = @y + (1 * move(command))
     end
   end
 
