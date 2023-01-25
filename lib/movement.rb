e844e7bd5aec95ca8052b909c94d17b38609f431
John Vicenzi <john.vicenzi@simplybusiness.com>
2023-01-25 08:14:42 -0500

    switch to case statement

--- lib/movement.rb
+++ lib/movement.rb
@@ -6,20 +6,17 @@ class Movement
   def move(command, direction, start_x, start_y)
     final_pos = [start_x, start_y]
     command.tap { |move_command|
-      if direction == 'n'
+      case direction
+      when 'n'
         final_pos[1] = final_pos[1] + 1 if move_command == 'f'
         final_pos[1] = final_pos[1] - 1 if move_command == 'b'
-      end
-      if direction == 's'
-
+      when 's'
         final_pos[1] = final_pos[1] - 1 if move_command == 'f'
         final_pos[1] = final_pos[1] + 1 if move_command == 'b'
-      end
-      if direction == 'e'
+      when 'e'
         final_pos[0] = final_pos[0] + 1 if move_command == 'f'
         final_pos[0] = final_pos[0] - 1 if move_command == 'b'
-      end
-      if direction == 'w'
+      when 'w'
         final_pos[0] = final_pos[0] - 1 if move_command == 'f'
         final_pos[0] = final_pos[0] + 1 if move_command == 'b'
       end
