696ef537d6823fe553e04c61b337d7f5f1321f30
John Vicenzi <john.vicenzi@simplybusiness.com>
2023-01-25 08:37:48 -0500

    refactored final_pos

--- lib/movement.rb
+++ lib/movement.rb
@@ -6,21 +6,21 @@ class MarsRover
 
   def move(command, direction, start_x, start_y)
     current_position = CurrentPosition.new(direction, start_x, start_y)
-    final_pos = [start_x, start_y]
+    final_pos = [current_position.x, current_position.y]
     command.tap { |move_command|
-      case direction
+      case current_position.direction
       when 'n'
-        final_pos[1] = final_pos[1] + 1 if move_command == 'f'
-        final_pos[1] = final_pos[1] - 1 if move_command == 'b'
+        final_pos[1] = current_position.y + 1 if move_command == 'f'
+        final_pos[1] = current_position.y - 1 if move_command == 'b'
       when 's'
-        final_pos[1] = final_pos[1] - 1 if move_command == 'f'
-        final_pos[1] = final_pos[1] + 1 if move_command == 'b'
+        final_pos[1] = current_position.y - 1 if move_command == 'f'
+        final_pos[1] = current_position.y + 1 if move_command == 'b'
       when 'e'
-        final_pos[0] = final_pos[0] + 1 if move_command == 'f'
-        final_pos[0] = final_pos[0] - 1 if move_command == 'b'
+        final_pos[0] = current_position.x + 1 if move_command == 'f'
+        final_pos[0] = current_position.x - 1 if move_command == 'b'
       when 'w'
-        final_pos[0] = final_pos[0] - 1 if move_command == 'f'
-        final_pos[0] = final_pos[0] + 1 if move_command == 'b'
+        final_pos[0] = current_position.x - 1 if move_command == 'f'
+        final_pos[0] = current_position.x + 1 if move_command == 'b'
       end
     }
     final_pos
