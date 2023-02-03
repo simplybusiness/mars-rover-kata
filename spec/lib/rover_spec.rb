7802ae41f36dcbb9825f1a7b6a46ab2996aa4a84
Victor Palombo Silvano <victorpsilvano@gmail.com>
2023-02-03 10:16:44 -0300

    red: Rover doesn't have a direction attribute

--- spec/lib/rover_spec.rb
+++ spec/lib/rover_spec.rb
@@ -1,8 +1,12 @@
 RSpec.describe Rover do
-  subject(:rover) { described_class.new(x: 0, y: 0) }
+  subject(:rover) { described_class.new(x: 0, y: 0, direction: 'N') }
 
   it 'has a starting point (x,y)' do
     expect(rover.x).to eq(0)
     expect(rover.y).to eq(0)
+  end
+
+  it 'has a starting the direction (N,S,E,W) it is facing' do
+    expect(rover.direction).to eq('N')
   end
 end
