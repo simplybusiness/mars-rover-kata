5dd363bf520568423b24482e0e315058d2d3a899
Victor Palombo Silvano <victorpsilvano@gmail.com>
2023-02-03 10:12:42 -0300

    red: Rover class doesn't have the starting points

--- spec/lib/rover_spec.rb
+++ spec/lib/rover_spec.rb
@@ -1,2 +1,8 @@
 RSpec.describe Rover do
+  subject(:rover) { described_class.new(x: 0, y: 0) }
+
+  it 'has a starting point (x,y)' do
+    expect(rover.x).to eq(0)
+    expect(rover.y).to eq(0)
+  end
 end
