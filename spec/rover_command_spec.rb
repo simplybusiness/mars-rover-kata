f5c18facd573c60cb0e4493a55c49d0ff07a1c70
Jr-Han Tai <jr-han.tai@simplybusiness.com>
2023-02-10 12:04:50 -0500

    add test for the 1st requirement

--- spec/rover_command_spec.rb
+++ spec/rover_command_spec.rb
@@ -1,5 +1,22 @@
+class Rover
+    def initialize(starting_point)
+    end
+
+    def current_position
+        [0,0]
+    end
+end
+
 RSpec.describe '1 + 1' do
     it 'equals 2' do
         expect(1 + 1).to eq(2)
     end
+
+    it 'knows it current position' do
+        starting_point = [0, 0]
+        rover = Rover.new(starting_point)
+        expect(rover.current_position). to eq([0, 0])
+    end
+
+    it 'knows it current direction'
 end
