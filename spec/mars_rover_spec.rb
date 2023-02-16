41a031934ecf5ceb67aba6dfda9e65fb4833a782
Sebastian Torrente <sebastian.torrente@simplybusiness.co.uk>
2023-02-16 14:10:36 +0000

    removed let from mars_rover_spec and put the starting rover position inside each describe context

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -3,7 +3,6 @@ require_relative '../coordinates'
 require_relative '../mars_rover'
 
 RSpec.describe MarsRover do
-  let(:rover) { MarsRover.new(x: 0, y: 0, orientation: "N") }
 
   describe 'position' do
     it 'knows its current position' do
@@ -13,43 +12,61 @@ RSpec.describe MarsRover do
     it 'returns North' do
       expect(rover.orientation).to eq("N")
     end
+
+    def rover
+      MarsRover.new(x: 0, y: 0, orientation: "N")
+    end
   end
 
   describe 'movement for a North facing Mars Rover' do
     it 'moves the rover forward by one space' do
+      rover = north_facing_rover
+      
       rover.execute("f")
 
       expect(rover.position).to eq(Coordinates.new(x: 0, y: 1))
     end
 
     it 'moves the rover backwards by one space' do
+      rover = north_facing_rover
+
       rover.execute("b")
 
       expect(rover.position).to eq(Coordinates.new(x: 0, y: -1))
     end
 
     it 'move multiple steps forward' do
+      rover = north_facing_rover
+
       rover.execute("fff")
 
       expect(rover.position).to eq(Coordinates.new(x: 0, y: 3))
     end
 
     it 'moves mulitple steps back' do
+      rover = north_facing_rover
+
       rover.execute("bbb")
 
       expect(rover.position).to eq(Coordinates.new(x: 0, y: -3))
     end
 
     it 'will ignore the unknown command' do
+      rover = north_facing_rover
+
       rover.execute("u")
 
       expect(rover.position).to eq(Coordinates.new(x: 0, y: 0))
+    end
+
+    def north_facing_rover
+      MarsRover.new(x: 0, y: 0, orientation: "N")
     end
   end
 
   describe 'movement for a East facing Mars Rover' do
     it 'moves the rover forward by one space' do
-      rover = MarsRover.new(x: 0, y: 0, orientation: "E")
+      rover = east_facing_rover
 
       rover.execute("f")
 
@@ -57,11 +74,31 @@ RSpec.describe MarsRover do
     end
 
     it 'moves multiples step forward' do
-      rover = MarsRover.new(x: 0, y: 0, orientation: "E")
+      rover = east_facing_rover
 
       rover.execute("fff")
 
-      expect(rover.position). to eq(Coordinates.new(x:3, y:0))
+      expect(rover.position).to eq(Coordinates.new(x:3, y:0))
+    end
+
+    it 'moves the rover backwards by one space' do
+      rover = east_facing_rover
+
+      rover.execute("b")
+
+      expect(rover.position).to eq(Coordinates.new(x:-1, y:0))
+    end
+
+    it 'moves multiples steps backwards' do
+      rover = east_facing_rover
+
+      rover.execute("bbb")
+
+      expect(rover.position).to eq(Coordinates.new(x:-3, y:0))
+    end
+
+    def east_facing_rover
+      MarsRover.new(x: 0, y: 0, orientation: "E")
     end
   end
 end
