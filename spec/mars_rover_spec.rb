31dfa9348c61eff0b23ebf889ffbc92034e5b250
natekennedysb <nathan.kennedy@simplybusiness.com>
2023-05-16 13:54:49 -0400

    feat: added baseline tests

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -3,3 +3,14 @@ describe "1+1" do
     expect(1+1).to eq(2)
   end
 end
+
+describe "on bootup" do
+  it "starts at the origin and faces N" do
+    expect(rover.starting_point).to eq([coords.x, coords.y])
+    expect(rover.direction).to eq("N")
+  end
+
+  it "does not start at the origin and faces N"
+
+  it "is facing W"
+end
