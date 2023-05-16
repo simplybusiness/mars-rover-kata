bc21c178fb54fe0d3a6e202367620978a3816413
Jr-Han Tai <jr-han.tai@simplybusiness.com>
2023-05-16 14:04:07 -0400

    Add class MarsRover

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,3 +1,20 @@
+require 'ostruct'
+
+class MarsRover
+  def initialize(coords, direction)
+    @coords = coords
+    @direction = direction
+  end
+
+  def starting_point
+    [@coords.x, @coords.y]
+  end
+
+  def direction
+    @direction
+  end
+end
+
 describe "1+1" do
   it "equals 2" do
     expect(1+1).to eq(2)
@@ -6,6 +23,8 @@ end
 
 describe "on bootup" do
   it "starts at the origin and faces N" do
+    rover = MarsRover.new(OpenStruct.new(x: 0, y: 0), "N")
+    coords = OpenStruct.new(x: 0, y: 0)
     expect(rover.starting_point).to eq([coords.x, coords.y])
     expect(rover.direction).to eq("N")
   end
