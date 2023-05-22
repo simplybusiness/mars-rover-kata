acf741c929a697b3d91db296cd20edc19f53276a
natekennedysb <nathan.kennedy@simplybusiness.com>
2023-05-22 10:35:14 -0400

    refactor: added spaces

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -15,6 +15,7 @@ end
 
 describe "1+1" do
   it "equals 2" do
+
     expect(1+1).to eq(2)
   end
 end
@@ -22,6 +23,7 @@ end
 describe "on bootup" do
   it "starts at the origin and faces N" do
     rover = MarsRover.new(OpenStruct.new(x: 0, y: 0), "N")
+
     coords = OpenStruct.new(x: 0, y: 0)
     expect(rover.starting_point).to eq([coords.x, coords.y])
     expect(rover.direction).to eq("N")
@@ -29,11 +31,13 @@ describe "on bootup" do
 
   it "does not start at the origin and faces N" do
     rover = MarsRover.new(OpenStruct.new(x: 1, y: 1), "N")
+
     expect(rover.starting_point).to eq([1, 1])
   end
 
   it "is facing W" do
     rover = MarsRover.new(OpenStruct.new(x: 0, y: 0), "W")
+
     expect(rover.direction).to eq("W")
   end
 end
