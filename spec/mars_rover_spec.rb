2ffce5dd131dc5f0df1ba3b2ca2dfc866e31adc9
natekennedysb <nathan.kennedy@simplybusiness.com>
2023-05-22 10:39:25 -0400

    refactor: changed quotes used

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,31 +1,31 @@
 require 'ostruct'
 require_relative '../lib/mars_rover'
 
-describe "1+1" do
-  it "equals 2" do
+describe '1+1' do
+  it 'equals 2' do
 
     expect(1+1).to eq(2)
   end
 end
 
-describe "on bootup" do
-  it "starts at the origin and faces N" do
-    rover = MarsRover.new(OpenStruct.new(x: 0, y: 0), "N")
+describe 'on bootup' do
+  it 'starts at the origin and faces N' do
+    rover = MarsRover.new(OpenStruct.new(x: 0, y: 0), 'N')
 
     coords = OpenStruct.new(x: 0, y: 0)
     expect(rover.starting_point).to eq([coords.x, coords.y])
-    expect(rover.direction).to eq("N")
+    expect(rover.direction).to eq('N')
   end
 
-  it "does not start at the origin and faces N" do
-    rover = MarsRover.new(OpenStruct.new(x: 1, y: 1), "N")
+  it 'does not start at the origin and faces N' do
+    rover = MarsRover.new(OpenStruct.new(x: 1, y: 1), 'N')
 
     expect(rover.starting_point).to eq([1, 1])
   end
 
-  it "is facing W" do
-    rover = MarsRover.new(OpenStruct.new(x: 0, y: 0), "W")
+  it 'is facing W' do
+    rover = MarsRover.new(OpenStruct.new(x: 0, y: 0), 'W')
 
-    expect(rover.direction).to eq("W")
+    expect(rover.direction).to eq('W')
   end
 end
