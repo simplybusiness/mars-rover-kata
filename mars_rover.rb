254e17aef5f2a50589cb502318ea706af851db3b
Andrew Davis <andrew.davis@simplybusiness.co.uk>
2023-07-19 15:59:42 +0100

    added new mars rover class and ensured test passed

---
+++ mars_rover.rb
@@ -0,0 +1,9 @@
+require 'ostruct'
+class MarsRover
+
+
+
+  def starting_point
+    OpenStruct.new(x: 0, y: 0)
+  end
+end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,10 +1,9 @@
 require 'ostruct'
-describe 'Mars rover' do
-  class MarsRover
+require_relative '../mars_rover'
 
-  end
+describe 'Mars rover' do
+
   it 'has a starting point' do
-    pending('Homework')
 
     mars_rover = MarsRover.new
     expect(mars_rover.starting_point).to eq(OpenStruct.new(x: 0, y: 0))
