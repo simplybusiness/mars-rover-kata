ac346a73bdf8f7b0a5ee4643b370ea8553e9db31
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-07-19 15:49:43 +0100

    Added homework for homework for cohort 3 - get the test passing.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,4 +1,11 @@
+require 'ostruct'
 describe 'Mars rover' do
-  it 'has a starting point'
+  it 'has a starting point' do
+    pending('Homework')
+
+    mars_rover = MarsRover.new
+    expect(mars_rover.starting_point).to eq(OpenStruct.new(x: 0, y: 0))
+  end
+
   it 'faces a particular direction (N, E, S, W)'
 end
