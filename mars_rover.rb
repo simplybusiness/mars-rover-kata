7ac332141c7f773ea83ec88daa5a4346025330a0
Ritik Agarwal <ritik.agarwal@simplybusiness.co.uk>
2023-07-24 15:54:58 +0100

    Some refactoring

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,12 +1,9 @@
 require 'ostruct'
 class MarsRover
-  attr_accessor :direction, :starting_point
+  attr_reader :direction, :starting_point
   def initialize(direction: 'N' , starting_point: OpenStruct.new(x: 0,y: 0))
     @direction = direction
     @starting_point = starting_point
-  end
-  def starting_point
-    @starting_point
   end
   def execute(commands)
     @starting_point = OpenStruct.new(x: @starting_point.x,y: @starting_point.y + 1)
