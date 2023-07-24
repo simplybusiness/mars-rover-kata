c565d2ac3167b47c40f775a322b354fccce7a4b4
Ritik Agarwal <ritik.agarwal@simplybusiness.co.uk>
2023-07-24 15:00:59 +0100

    Removed duplication

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -3,19 +3,19 @@ require_relative '../mars_rover'
 
 describe 'Mars rover' do
 
-  it 'has a starting point' do
+  let(:mars_rover) do
+    MarsRover.new
+  end
 
-    mars_rover = MarsRover.new
+  it 'has a starting point' do
     expect(mars_rover.starting_point).to eq(OpenStruct.new(x: 0, y: 0))
   end
 
   it 'faces a particular direction (N, E, S, W)' do
-    mars_rover = MarsRover.new
     expect(mars_rover.direction).to eq('N')
   end
 
   it 'faces any particular direction' do
-    mars_rover = MarsRover.new
     mars_rover.direction = 'S'
     expect(mars_rover.direction).to eq('S')
   end
