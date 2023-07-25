2d4996bf5afb09d2a116ee94f5974dfc50b1a7c2
Andrew Davis <andrew.davis@simplybusiness.co.uk>
2023-07-25 11:19:52 +0100

    moves forward and moves backward

--- mars_rover.rb
+++ mars_rover.rb
@@ -8,8 +8,43 @@ class MarsRover
   def starting_point
     @starting_point
   end
-  def execute(commands)
-    @starting_point = OpenStruct.new(x: @starting_point.x,y: @starting_point.y + 1)
+  def execute(commands: [])
+
+    x = @starting_point.x
+    y = @starting_point.y
+
+
+    if commands.include? 'f'
+      case @direction
+      when 'N'
+        y = @starting_point.y + 1
+      when 'S'
+        y = @starting_point.y - 1
+      when 'E'
+        x = @starting_point.x - 1
+      when 'W'
+        x = @starting_point.x + 1
+      else
+        x = x
+        y = y
+      end
+    elsif commands.include? 'b'
+      case @direction
+      when 'N'
+        y = @starting_point.y - 1
+      when 'S'
+        y = @starting_point.y + 1
+      when 'E'
+        x = @starting_point.x + 1
+      when 'W'
+        x = @starting_point.x - 1
+      else
+        x = x
+        y = y
+      end
+    end
+
+    @starting_point = OpenStruct.new(x: x,y: y)
   end
 
 end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -5,6 +5,8 @@ describe 'Mars rover' do
 
   let(:mars_rover) { MarsRover.new }
 
+
+
   it 'has a starting point' do
     expect(mars_rover.starting_point).to eq(OpenStruct.new(x: 0, y: 0))
   end
@@ -26,12 +28,34 @@ describe 'Mars rover' do
     expect(mars_rover).to respond_to(:execute)
   end
 
-  it 'moves forward when facing north' do
-    mars_rover = MarsRover.new(starting_point: OpenStruct.new(x:2, y: 5))
-    mars_rover.execute(['f'])
-    expect(mars_rover.starting_point).to eq(OpenStruct.new(x: 2, y: 6))
+
+
+  context "moves forward" do
+    [OpenStruct.new(direction: "N", start: OpenStruct.new(x:2, y: 5), end: OpenStruct.new(x:2, y: 6)),
+     OpenStruct.new(direction: "S", start: OpenStruct.new(x:2, y: 5), end: OpenStruct.new(x:2, y: 4)),
+     OpenStruct.new(direction: "E", start: OpenStruct.new(x:2, y: 5), end: OpenStruct.new(x:1, y: 5)),
+     OpenStruct.new(direction: "W", start: OpenStruct.new(x:2, y: 5), end: OpenStruct.new(x:3, y: 5))
+    ].each do |hash|
+      it "moves when facing (#{hash.direction}) " do
+        mars_rover = MarsRover.new(direction: hash.direction, starting_point: hash.start)
+        mars_rover.execute(commands: ['f'])
+        expect(mars_rover.starting_point).to eq(hash.end)
+      end
+    end
   end
-  it 'moves forward when facing south'
-  it 'moves forward when facing east'
-  it 'moves forward when facing west'
+
+  context "moves backward" do
+    [OpenStruct.new(direction: "N", start: OpenStruct.new(x:2, y: 5), end: OpenStruct.new(x:2, y: 4)),
+     OpenStruct.new(direction: "S", start: OpenStruct.new(x:2, y: 5), end: OpenStruct.new(x:2, y: 6)),
+     OpenStruct.new(direction: "E", start: OpenStruct.new(x:2, y: 5), end: OpenStruct.new(x:3, y: 5)),
+     OpenStruct.new(direction: "W", start: OpenStruct.new(x:2, y: 5), end: OpenStruct.new(x:1, y: 5))
+    ].each do |hash|
+      it "moves when facing (#{hash.direction}) " do
+        mars_rover = MarsRover.new(direction: hash.direction, starting_point: hash.start)
+        mars_rover.execute(commands: ['b'])
+        expect(mars_rover.starting_point).to eq(hash.end)
+      end
+    end
+  end
+
 end
