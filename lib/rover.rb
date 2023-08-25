5e624f2e2c075b3cafe977e32660a042a7f2feff
sblouet <stephen.blouet@simplybusiness.co.uk>
2023-08-25 09:04:51 +0100

    refactored tests

--- lib/rover.rb
+++ lib/rover.rb
@@ -2,7 +2,9 @@ class Rover
     attr_reader :position, :direction
     NORTH = 'N'
     LEFT = 'l'
+    RIGHT = 'r'
     WEST = 'W'
+    EAST = 'E'
    
     def initialize (coordinates:[0,0], direction: NORTH)
         @position = coordinates
@@ -11,8 +13,11 @@ class Rover
 
     def move (route)
       if route.length > 0
-        if route == LEFT
+        case route
+        when LEFT 
           @direction = WEST
+        when RIGHT
+          @direction = EAST
         else
           raise ArgumentError
         end
