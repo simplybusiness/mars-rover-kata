73f155f760301c875bb27d4779e4440adaf1d72d
Colum O'Donovan <colum.odonovan@simplybusiness.co.uk>
2023-09-01 15:34:18 +0100

    Added initial mars rover class and tests

--- spec/mars_rovers_spec.rb
+++ spec/mars_rovers_spec.rb
@@ -1,5 +1,15 @@
-describe '1 + 1'  do
+require_relative '../lib/mars_rover'
+
+describe MarsRover do
   it 'equals 2 ' do
     expect(1 + 1).to eq 2
   end
+
+  it 'has a starting position of (2,3)'
+
+  it 'has a starting facing direction of N'
+
+  it 'has a starting position of (1,2) and a facing direction of S'
+
+  it 'does not have a starting direction of (1,4)'
 end
