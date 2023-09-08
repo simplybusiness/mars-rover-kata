0d250e2e39f636dc2a16af6b735c9612f139ed28
Colum O'Donovan <colum.odonovan@simplybusiness.co.uk>
2023-09-08 15:15:13 +0100

    Added initial commands functionality

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -2,10 +2,14 @@
 require 'ostruct'
 
 class MarsRover
-  attr_reader :point, :direction
+  attr_reader :point, :direction, :commands
 
   def initialize(direction, point)
     @point = point
     @direction = direction
+  end
+
+  def add_commands(commands)
+    @commands = commands
   end
 end
--- spec/mars_rovers_spec.rb
+++ spec/mars_rovers_spec.rb
@@ -24,4 +24,14 @@ describe MarsRover do
     expect(mars_rover.point).to eq OpenStruct.new(x: 1, y: 2)
     expect(mars_rover.direction).to eq 'S'
   end
+
+  it 'receives a list of commands of [f,f,l,r,b]' do
+    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2))
+    mars_rover.add_commands(%w[f f l r b])
+
+    expect(mars_rover.commands).to eq %w[f f l r b]
+  end
+
+
+  it 'executes no commands when an empty list of commands is received'
 end
