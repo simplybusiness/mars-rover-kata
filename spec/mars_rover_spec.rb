d929ccf39b3b7a53884c3118bbf9b64b6838f405
Jing Wu <jing.wu@simplybusiness.co.uk>
2023-09-08 10:57:15 +0100

    spec: add small test for first feature

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,4 +2,7 @@ RSpec.describe "2 + 2" do
   it 'equals 4' do
     expect(2+2).to eq 4
   end
+  it 'initial starting point should be given'
+  it 'initial direction should be given'
+  it 'initial direction should be fall into the range of (N,W,S,E) with Capital letters'
 end
