29a2f6443a3b7ab52162272dc66261af05cacb5c
Jing Wu <jing.wu@simplybusiness.co.uk>
2023-09-11 19:01:57 +0100

    spec: add rspec tests for second requirement

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -7,9 +7,19 @@ RSpec.describe "2 + 2" do
     end
 
     context 'with invalid inputs' do
-      it 'raise an error when x is invalid'
-      it 'raise an error when y is invalid'
-      it 'raise an error when direction is invalid'
+      it 'raises an error when x is invalid'
+      it 'raises an error when y is invalid'
+      it 'raises an error when direction is invalid'
+    end
+  end
+  describe '#receives command list' do
+    context 'receives valid command list' do
+      it 'receives empty list and rover stays at the same position'
+      it 'receives with a list of characters in (f, b, l, r)'
+    end
+
+    context 'receive invalid command list' do
+      it 'raises an error when command list contain invalid character(s)'
     end
   end
 end
