7605ea85a6c8de2180751d48953be6e5c83adc77
Jing Wu <jing.wu@simplybusiness.co.uk>
2023-09-11 18:42:10 +0100

    spec: add rspec tests for first requirement

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,8 +1,15 @@
 RSpec.describe "2 + 2" do
-  it 'equals 4' do
-    expect(2+2).to eq 4
+  describe '#initialize' do
+    context 'with valid inputs' do
+      it 'sets the x correctly'
+      it 'sets the y correctly'
+      it 'sets the direction correctly'
+    end
+
+    context 'with invalid inputs' do
+      it 'raise an error when x is invalid'
+      it 'raise an error when y is invalid'
+      it 'raise an error when direction is invalid'
+    end
   end
-  it 'initial starting point should be given'
-  it 'initial direction should be given'
-  it 'initial direction should be fall into the range of (N,W,S,E) with Capital letters'
 end
