1eb1f2cd992b842c305b6d953f557ae29d826f23
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-09-14 12:19:20 +0100

    Added a failing test.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,12 +1,18 @@
 RSpec.describe "Mars Rover" do
+  class MarsRover
 
+  end
   it 'has an initial starting point (x, y)'
 
   it 'has a direction (N, E, S, W)'
 
   describe '#initialize' do
     context 'with valid inputs' do
-      it 'sets the x correctly'
+      it 'sets the x correctly' do
+        pending('TO DO')
+        mars_rover = MarsRover.new
+        expect(mars_rover.x).to eq(0)
+      end
       it 'sets the y correctly'
       it 'sets the direction correctly'
     end
