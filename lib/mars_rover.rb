edeb49c9e2b674ac9c3bccf3c374f7ce22f95469
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 14:16:12 +0100

    We no longer need the default setting of direction.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,7 +1,7 @@
 class MarsRover
   attr_reader :current_position, :direction
 
-  def initialize(starting_position:, direction: 'S')
+  def initialize(starting_position:, direction:)
     @current_position = starting_position
     @direction = direction
   end
