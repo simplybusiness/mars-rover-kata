37b10341309b967428ba003918ca9bbce7c04b01
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 14:12:32 +0100

    Pushed a detail up the callstack.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,11 +1,8 @@
 class MarsRover
-  attr_reader :current_position
+  attr_reader :current_position, :direction
 
   def initialize(starting_position:)
     @current_position = starting_position
-  end
-
-  def direction
-    'S'
+    @direction = 'S'
   end
 end
