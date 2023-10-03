1fea33d58207457160cb79a312eee2c9accdf901
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 14:02:07 +0100

    Migrated a client (the tests).

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'Operating a Mars Rover' do
   it 'has a starting position at the origin' do
     mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 0, y: 0))
 
-    expect(mars_rover.starting_position).to eq(OpenStruct.new(x: 0, y: 0))
+    expect(mars_rover.current_position).to eq(OpenStruct.new(x: 0, y: 0))
   end
 
   it 'has a starting position anywhere away from an origin'
