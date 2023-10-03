468c6a8cedf893f07cb14e0b7f9b7505da3ad53d
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 14:06:31 +0100

    Now the Mars Rover can have a position anywhere, away from the origin.

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Operating a Mars Rover' do
     attr_reader :current_position
 
     def initialize(starting_position: nil)
-      @current_position = OpenStruct.new(x: 0, y: 0)
+      @current_position = starting_position
     end
   end
 
@@ -14,7 +14,11 @@ RSpec.describe 'Operating a Mars Rover' do
     expect(mars_rover.current_position).to eq(OpenStruct.new(x: 0, y: 0))
   end
 
-  it 'has a starting position anywhere away from an origin'
+  it 'has a starting position anywhere away from an origin' do
+    mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 1, y: -1))
+
+    expect(mars_rover.current_position).to eq(OpenStruct.new(x: 1, y: -1))
+  end
 
   it 'has a direction'
 
