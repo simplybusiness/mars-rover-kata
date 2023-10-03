755515d429b92ca76a1de3d56d3966ae8f0d78d5
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 14:15:42 +0100

    Bubbled some details up towards the test.

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -2,19 +2,19 @@ require 'ostruct'
 require_relative '../lib/mars_rover'
 RSpec.describe 'Operating a Mars Rover' do
   it 'has a starting position at the origin' do
-    mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 0, y: 0))
+    mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 0, y: 0), direction: 'E')
 
     expect(mars_rover.current_position).to eq(OpenStruct.new(x: 0, y: 0))
   end
 
   it 'has a starting position anywhere away from an origin' do
-    mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 1, y: -1))
+    mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 1, y: -1), direction: 'N')
 
     expect(mars_rover.current_position).to eq(OpenStruct.new(x: 1, y: -1))
   end
 
   it 'has a direction' do
-    mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 1, y: -1))
+    mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 1, y: -1), direction: 'S')
 
     expect(mars_rover.direction).to eq('S')
   end
