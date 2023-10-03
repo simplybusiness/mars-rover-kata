f0dae333ab1b32cb35f632c782e58fbd7a45ca30
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 14:24:18 +0100

    Migrated a test to using the new Coordinates class.

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -1,8 +1,9 @@
 require 'ostruct'
+require_relative '../lib/coordinates'
 require_relative '../lib/mars_rover'
 RSpec.describe 'Operating a Mars Rover' do
   it 'has a starting position at the origin' do
-    mars_rover = MarsRover.new(starting_position: OpenStruct.new(x: 0, y: 0), direction: 'E')
+    mars_rover = MarsRover.new(starting_position: Coordinates.new(x: 0, y: 0), direction: 'E')
 
     expect(mars_rover.current_position.x).to eq(0)
     expect(mars_rover.current_position.y).to eq(0)
