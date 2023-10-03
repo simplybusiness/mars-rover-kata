34c2e99ed93dd1f873130e433acc9ce44af91919
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 15:15:33 +0100

    Removed some whitespace.

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -27,7 +27,6 @@ RSpec.describe 'Operating a Mars Rover' do
 
       expect(mars_rover.direction).to eq(direction)
     end
-
   end
 
   it 'cannot be operated when it has a direction that is not north, east, south or west' do
