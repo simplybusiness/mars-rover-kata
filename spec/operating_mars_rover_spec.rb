e416580d132c67a8ad6fe248a5466594c6e04fc4
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 13:56:05 +0100

    We can land a Mars Rover at an origin.

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -1,5 +1,16 @@
+require 'ostruct'
 RSpec.describe 'Operating a Mars Rover' do
-  it 'has a starting position at the origin'
+  class MarsRover
+    def starting_position
+      OpenStruct.new(x: 0, y: 0)
+    end
+  end
+
+  it 'has a starting position at the origin' do
+    mars_rover = MarsRover.new
+
+    expect(mars_rover.starting_position).to eq(OpenStruct.new(x: 0, y: 0))
+  end
 
   it 'has a starting position anywhere away from an origin'
 
