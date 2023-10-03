419b1ebbb2a3748c8fab8ad4287ed369f77977f1
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-03 13:53:43 +0100

    Added a test list for the first requirement.

---
+++ spec/operating_mars_rover_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe 'Operating a Mars Rover' do
+  it 'has a starting position at the origin'
+
+  it 'has a starting position anywhere away from an origin'
+
+  it 'has a direction'
+
+  it 'cannot be operated when it has a direction that is not north, east, south or west'
+
+  it 'cannot be operated when it is not given a starting position'
+end
