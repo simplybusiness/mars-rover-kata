a1e6c3f04944c863ea1ee16e38bec88e3b03c1ed
sblouet <stephen.blouet@simplybusiness.co.uk>
2023-10-04 11:16:16 +0100

    created mars rover class and spec

---
+++ spec/mars_rover_spec.rb
@@ -0,0 +1,13 @@
+# Req 1: You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
+require_relative "spec_helper"
+require_relative "../lib/mars_rover"
+
+describe MarsRover do
+    describe "#initialise" do
+        xit "has an x coordinate of 0 and a y coordinate of 0 when initialised without a start position" do
+        end
+
+        xit "has an x coordinate and y coordinate equal to the start position passed on initialisation" do
+        end
+    end
+end
