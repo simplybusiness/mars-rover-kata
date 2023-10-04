32e388609e9d97c5464a2591fca2bcb63831a5c5
Vladyslav Farion <vladyslav.farion@simplybusiness.co.uk>
2023-10-04 14:00:52 +0300

    refactor: simplify checking starting point type condition

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -2,9 +2,7 @@ class MarsRover
     attr_reader :position
 
     def initialize(starting_point)
-        if !starting_point[0].is_a?(Integer) || !starting_point[1].is_a?(Integer)
-            raise ArgumentError
-        end
+        raise ArgumentError unless starting_point[0].is_a?(Integer) && starting_point[1].is_a?(Integer)
 
         @position = starting_point
     end
