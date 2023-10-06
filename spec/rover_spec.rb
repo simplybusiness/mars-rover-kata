49014f103ffdf867d137f71f7148bce58392eeda
sblouet <stephen.blouet@simplybusiness.co.uk>
2023-10-06 16:23:18 +0100

    refactor: test now uses coordinate data type and coordinate_position field on rover class

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -10,9 +10,10 @@ describe Rover do
     end
 
     it 'has position matching the coordinates it was initialized with' do
-      rover = Rover.new(coordinates:[3,5])
-      expect(rover.position[0]).to eq(3)
-      expect(rover.position[1]).to eq(5)
+      start_position = Coordinate.new(x=3, y=5)
+      rover = Rover.new(coordinates:start_position)
+      expect(rover.coordinate_position.x).to eq(3)
+      expect(rover.coordinate_position.y).to eq(5)
     end
 
     it 'has initially faces North when not given any direction' do
