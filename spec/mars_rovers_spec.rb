6f66303da014c4ffe354bbf82524f3e567c97232
Colum O'Donovan <colum.odonovan@simplybusiness.co.uk>
2023-10-09 10:49:41 +0100

    Removed tests that check correct commands are added

--- spec/mars_rovers_spec.rb
+++ spec/mars_rovers_spec.rb
@@ -25,36 +25,6 @@ describe MarsRover do
     expect(mars_rover.direction).to eq 'S'
   end
 
-  it 'receives a list of commands of [f,f,l,r,b]' do
-    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2))
-    mars_rover.add_commands(%w[f f l r b])
-
-    expect(mars_rover.commands).to eq %w[f f l r b]
-  end
-
-  it 'executes no commands when an empty list of commands is received' do
-    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2))
-    mars_rover.add_commands([])
-
-    expect(mars_rover.commands).to eq []
-  end
-
-  it 'does not add any commands if the commands are not in Array' do
-    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2))
-    mars_rover.add_commands('f f b')
-
-    expect(mars_rover.commands).to eq []
-  end
-
-  it 'overwrites the the first set of commands when two arrays of commands are added ' do
-    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2))
-
-    mars_rover.add_commands(%w[f f l r b])
-    mars_rover.add_commands(%w[e e l r b])
-
-    expect(mars_rover.commands).to eq %w[e e l r b]
-  end
-
   it 'does not process unrecognised commands (e.g. command y)'
 
   it 'moves forwards when facing north' do
