ffc62eacc8dda2839b01dd22cac9588e735649c5
Lucas Sigismondi <lucas.sigismondi@simplybusiness.co.uk>
2023-10-09 15:02:56 +0100

    test: add direction paramentert to initialize and initial direction tests

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,9 +1,10 @@
 class MarsRover
     attr_reader :position
 
-    def initialize(starting_point)
+    def initialize(starting_point, direction)
         raise ArgumentError.new('Starting point should have [x, y] coordinate') unless starting_point.length == 2  
         raise ArgumentError.new('Starting point should have Integer coordinates') unless starting_point[0].is_a?(Integer) && starting_point[1].is_a?(Integer)
+        raise ArgumentError.new('Direction is not valid, use one of (N,S,E,W)') unless ['N','E','S','W'].include?(direction)
 
         @position = starting_point
     end
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -9,16 +9,23 @@ describe MarsRover do
         end
 
         it "raise an error when initialized with non integers as start position" do
-            expect { MarsRover.new(["abc", "def"]) }.to raise_error(ArgumentError, 'Starting point should have Integer coordinates')
+            expect { MarsRover.new(["abc", "def"], "N") }.to raise_error(ArgumentError, 'Starting point should have Integer coordinates')
         end
 
         it 'raises an error when only one coordinate for starting point is provided' do
-            expect { MarsRover.new([0]) }.to raise_error(ArgumentError, 'Starting point should have [x, y] coordinate')
+            expect { MarsRover.new([0], "N") }.to raise_error(ArgumentError, 'Starting point should have [x, y] coordinate')
         end
 
         it "has an x coordinate and y coordinate equal to the start position passed on initialization" do
-            mars_rover = MarsRover.new([0, 0])
+            mars_rover = MarsRover.new([0, 0], "N")
             expect(mars_rover.position).to eq([0,0])
+        end
+
+        it "raises an error when direction passed is not N,S,E,W" do 
+            expect { MarsRover.new([0,0], "X") }.to raise_error(ArgumentError, 'Direction is not valid, use one of (N,S,E,W)')
+        end
+
+        xit "has a current direction equal to the direction passed on the initialization" do 
         end
     end
 end
