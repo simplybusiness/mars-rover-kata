a80e6303c5cbee514a4f58cfcf0dd1060aae1721
sblouet <stephen.blouet@simplybusiness.co.uk>
2023-10-12 13:21:20 +0100

    refactor: introduced x_position as a more descriptive name

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -1,8 +1,10 @@
 class Coordinate
-  attr_accessor :x, :y
+  attr_accessor :x, :y, :x_position, :y_position
 
   def initialize(x = 0, y = 0)
     @x = x
     @y = y
+    @x_position = x
+    @y_position = y
   end
 end
