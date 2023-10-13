46428a0eaedf211d9191eae397c06e493968eb4b
Colum O'Donovan <colum.odonovan@simplybusiness.co.uk>
2023-10-13 17:48:32 +0100

    Added point method calls to mars_rover

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -5,7 +5,7 @@ require_relative 'point'
 class MarsRover
   attr_reader :point, :direction, :new_point
 
-  def initialize(direction, point, new_point: nil)
+  def initialize(direction, point, new_point = Point.new(0,0))
     @point = point
     @new_point = new_point
     @direction = direction
@@ -30,20 +30,26 @@ class MarsRover
     case @direction
     when 'E'
       @point = OpenStruct.new(x: @point.x + 1, y: @point.y)
+      @new_point.move_x(1)
     when 'S'
       @point = OpenStruct.new(x: @point.x, y: @point.y - 1)
+      @new_point.move_y(-1)
     when 'W'
       @point = OpenStruct.new(x: @point.x - 1, y: @point.y)
+      @new_point.move_x(-1)
     when 'N'
       @point = OpenStruct.new(x: @point.x, y: @point.y + 1)
+      @new_point.move_y(1)
     end
   end
 
   def move_backward
     if @direction ==  'N'
       @point = OpenStruct.new(x: @point.x, y: @point.y - 1 )
+      @new_point.move_y(-1)
     elsif @direction == 'S'
       @point = OpenStruct.new(x: @point.x, y: @point.y + 1 )
+      @new_point.move_y(1)
     end
   end
 end
