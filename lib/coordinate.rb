52ce3a6625c7738ed03918264afb5aea9bb7cca6
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-13 10:09:25 +0100

    Parallel Change: added the new parameters.

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -1,7 +1,7 @@
 class Coordinate
   attr_accessor :x, :y, :x_position, :y_position
 
-  def initialize(x = 0, y = 0)
+  def initialize(x = 0, y = 0, x_pos: 0, y_pos: 0)
     @x = x
     @y = y
     @x_position = x
