1c176b1695fd6df61f119ba18e4d9d5c69ca9947
sblouet <stephen.blouet@simplybusiness.co.uk>
2023-10-13 11:13:36 +0100

    refactor: removed unnecessary code from coordinate

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -1,10 +1,8 @@
 class Coordinate
-  attr_accessor :x, :y, :x_position, :y_position
+  attr_accessor :x, :y
 
   def initialize(x = 0, y = 0, x_pos: 0, y_pos: 0)
     @x = x
     @y = y
-    @x_position = x
-    @y_position = y
   end
 end
