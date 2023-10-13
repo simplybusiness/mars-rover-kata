34f603177939e852689d025da6e683d49ec777d8
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-13 10:28:46 +0100

    Migrated Coordinate to using x_pos and y_pos

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -2,8 +2,8 @@ class Coordinate
   attr_accessor :x, :y, :x_position, :y_position
 
   def initialize(x = 0, y = 0, x_pos: 0, y_pos: 0)
-    @x = x
-    @y = y
+    @x = x_pos
+    @y = y_pos
     @x_position = x
     @y_position = y
   end
