318c5dbec59eccda27868741ce2ab1ff76589e5a
Colum O'Donovan <colum.odonovan@simplybusiness.co.uk>
2023-10-13 17:08:59 +0100

    Added pending tests for move_x

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -8,6 +8,24 @@ describe Point do
     expect(point.coordinates).to eq(([0, 0]))
   end
 
+  it 'moves the x coordinate by a specific number' do
+    pending("TODO")
+    point =  Point.new(0, 0)
+
+    point.move_x(1)
+
+    expect(point.coordinates).to eq(([1, 0]))
+  end
+
+  it 'does not move the x coordinate if the number is not an integer' do
+    pending("TODO")
+    point =  Point.new(0, 0)
+
+    point.move_x(1.0)
+
+    expect(point.coordinates).to eq(([0, 0]))
+  end
+
 end
 
 
