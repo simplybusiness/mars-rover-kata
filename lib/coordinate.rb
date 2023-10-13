b6881c1d49d3d98f5b1e2022c8006c376c5bd73f
sblouet <stephen.blouet@simplybusiness.co.uk>
2023-10-13 13:25:17 +0100

    rafector: (expand) added better named instance variables for coordinate x and y position

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -1,16 +1,11 @@
 class Coordinate
-  attr_accessor :x, :y
+  attr_accessor :x, :y, :x_position, :y_position
 
   def initialize(x = 0, y = 0, x_pos: 0, y_pos: 0)
     @x = x_pos
     @y = y_pos
+    @x_position = x_pos
+    @y_position = y_pos
   end
 
-  def x_position
-    @x
-  end
-
-  def y_position
-    @y
-  end
 end
