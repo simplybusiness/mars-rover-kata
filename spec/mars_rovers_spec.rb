6b1bd5c914476e74288c400e511ecee24ac010ed
Colum O'Donovan <colum.odonovan@simplybusiness.co.uk>
2023-10-13 17:53:22 +0100

    Added new point to more tests

--- spec/mars_rovers_spec.rb
+++ spec/mars_rovers_spec.rb
@@ -22,20 +22,22 @@ describe MarsRover do
   end
 
   it 'has a starting position of (1,2) and a facing direction of S' do
-    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2))
+    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2), Point.new(2,3))
 
     expect(mars_rover.point).to eq OpenStruct.new(x: 1, y: 2)
+    expect(mars_rover.new_point.coordinates).to eq [2,3]
     expect(mars_rover.direction).to eq 'S'
   end
 
   it 'does not process unrecognised commands (e.g. command y)'
 
   it 'moves forwards when facing north' do
-    mars_rover = MarsRover.new('N', OpenStruct.new(x: 0, y: 0))
+    mars_rover = MarsRover.new('N', OpenStruct.new(x: 0, y: 0), Point.new(0,0))
 
     mars_rover.execute_commands(%w[f])
 
     expect(mars_rover.point).to eq(OpenStruct.new(x: 0, y: 1))
+    expect(mars_rover.new_point.coordinates).to eq [0,1]
   end
 
   it 'moves forwards when facing south' do
