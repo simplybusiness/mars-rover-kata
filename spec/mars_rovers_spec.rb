a30eebbd441399afa599ede581dd2b378338a9fb
Colum O'Donovan <colum.odonovan@simplybusiness.co.uk>
2023-10-16 09:48:42 +0100

    Added direction to mars rover spec

--- spec/mars_rovers_spec.rb
+++ spec/mars_rovers_spec.rb
@@ -6,7 +6,7 @@ describe MarsRover do
   end
 
   it 'has a starting position of (2,3)' do
-    mars_rover = MarsRover.new( 'N', OpenStruct.new(x: 2, y: 3), Point.new(2,3))
+    mars_rover = MarsRover.new( 'N', OpenStruct.new(x: 2, y: 3), Point.new(2,3, 'N'))
 
     point = mars_rover.point
     new_point = mars_rover.new_point
@@ -16,13 +16,13 @@ describe MarsRover do
   end
 
   it 'has a starting facing direction of N' do
-    mars_rover = MarsRover.new( 'N', OpenStruct.new(x: 2, y: 3), Point.new(2,3))
+    mars_rover = MarsRover.new( 'N', OpenStruct.new(x: 2, y: 3), Point.new(2,3, 'N'))
 
     expect(mars_rover.direction).to eq 'N'
   end
 
   it 'has a starting position of (1,2) and a facing direction of S' do
-    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2), Point.new(2,3))
+    mars_rover = MarsRover.new('S', OpenStruct.new(x: 1, y: 2), Point.new(2,3, 'S'))
 
     expect(mars_rover.point).to eq OpenStruct.new(x: 1, y: 2)
     expect(mars_rover.new_point.coordinates).to eq '[2 3]'
@@ -32,7 +32,7 @@ describe MarsRover do
   it 'does not process unrecognised commands (e.g. command y)'
 
   it 'moves forwards when facing north' do
-    mars_rover = MarsRover.new('N', OpenStruct.new(x: 0, y: 0), Point.new(0,0))
+    mars_rover = MarsRover.new('N', OpenStruct.new(x: 0, y: 0), Point.new(0,0, 'N'))
 
     mars_rover.execute_commands(%w[f])
 
@@ -41,7 +41,7 @@ describe MarsRover do
   end
 
   it 'moves forwards when facing south' do
-    mars_rover = MarsRover.new('S', OpenStruct.new(x: 0, y: 1),  Point.new(0,1))
+    mars_rover = MarsRover.new('S', OpenStruct.new(x: 0, y: 1),  Point.new(0,1, 'S'))
 
     mars_rover.execute_commands(%w[f])
 
@@ -50,7 +50,7 @@ describe MarsRover do
   end
 
   it 'moves forwards when facing west' do
-    mars_rover = MarsRover.new('W', OpenStruct.new(x: 0, y: 0),  Point.new(0,0))
+    mars_rover = MarsRover.new('W', OpenStruct.new(x: 0, y: 0),  Point.new(0,0, 'W'))
 
     mars_rover.execute_commands(%w[f])
 
@@ -60,7 +60,7 @@ describe MarsRover do
   end
 
   it 'moves forwards when facing east' do
-    mars_rover = MarsRover.new('E', OpenStruct.new(x: 0, y: 0),  Point.new(0,0))
+    mars_rover = MarsRover.new('E', OpenStruct.new(x: 0, y: 0),  Point.new(0,0, 'E'))
 
     mars_rover.execute_commands(%w[f])
 
@@ -70,7 +70,7 @@ describe MarsRover do
   end
 
   it 'moves backwards when facing north' do
-    mars_rover = MarsRover.new('N', OpenStruct.new(x: 0, y: 1),  Point.new(0,1))
+    mars_rover = MarsRover.new('N', OpenStruct.new(x: 0, y: 1),  Point.new(0,1, 'N'))
 
     mars_rover.execute_commands(%w[b])
 
@@ -80,7 +80,7 @@ describe MarsRover do
   end
 
   it 'moves backwards when facing south' do
-    mars_rover = MarsRover.new('S', OpenStruct.new(x: 0, y: 0),  Point.new(0,0))
+    mars_rover = MarsRover.new('S', OpenStruct.new(x: 0, y: 0),  Point.new(0,0, 'S'))
 
     mars_rover.execute_commands(%w[b])
 
