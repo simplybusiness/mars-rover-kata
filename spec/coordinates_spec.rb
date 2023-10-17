f85bad7911669e62d9d9a87f3c01dd9f1bfa4d60
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-17 13:00:44 +0100

    Added tests for the equals method.

---
+++ spec/coordinates_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/coordinates'
+RSpec.describe Coordinates do
+  describe 'equals' do
+    it 'is symmetric'
+    it 'is associative'
+    it 'is transitive'
+    specify 'two coordinates with the same x- and y-coordinates are equal'
+    specify 'two coordinates with the same x-coordinates but different y are unequal'
+    specify 'two coordinates with the same y-coordinates but equal y are unequal'
+    ['(1, 1)', [1, 2], {x: 1, y: 2}, 1].each do |object_of_different_type|
+      specify "coordinates are not equal to an object of a different type e.g. #{object_of_different_type}"
+    end
+  end
+end
