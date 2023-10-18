8333331e3fedc49c723452571865ede4770bab5e
Eduard Vasilescu <115027304+eduard-sb@users.noreply.github.com>
2023-10-18 10:24:32 +0100

    feat: upgrade turn logic, and run rubocop fixer'

--- lib/rover.rb
+++ lib/rover.rb
@@ -2,64 +2,55 @@ require 'coordinates'
 require 'pry-byebug'
 
 class Rover
-    attr_reader :position
-    attr_reader :direction
+  attr_reader :position, :direction
 
-    MOVE_COMMANDS = ['f', 'b']
-    TURN_COMMANDS = ['r', 'l']
-    COORDINATES = ['N','E','S','W']
+  MOVE_COMMANDS = %w[f b]
+  TURN_COMMANDS = %w[r l]
+  COORDINATES = %w[N E S W]
 
-    def initialize(position, direction)
-        raise ArgumentError.new('Position should be a Coordinate data type') unless position.is_a?(Coordinates)
-        raise ArgumentError.new('Direction is not valid, use one of (N,S,E,W)') unless COORDINATES.include?(direction)
+  def initialize(position, direction)
+    raise ArgumentError, 'Position should be a Coordinate data type' unless position.is_a?(Coordinates)
+    raise ArgumentError, 'Direction is not valid, use one of (N,S,E,W)' unless COORDINATES.include?(direction)
 
-        @position = position
-        @direction = direction
+    @position = position
+    @direction = direction
+  end
+
+  def execute(commands)
+    raise ArgumentError, 'Commands should be of Char Array or String format' unless
+    (commands.is_a? Array) || (commands.is_a? String)
+
+    for i in 0...commands.length do
+      next unless (MOVE_COMMANDS.include? commands[i]) || (TURN_COMMANDS.include? commands[i])
+
+      move(commands[i]) if MOVE_COMMANDS.include? commands[i]
+      turn(commands[i]) if TURN_COMMANDS.include? commands[i]
     end
+  end
 
-    def execute(commands) 
-        raise ArgumentError.new("Commands should be of Char Array or String format") unless 
-        ((commands.is_a? Array) || (commands.is_a? String))
-
-        for i in 0...commands.length do
-            next unless (MOVE_COMMANDS.include? commands[i]) || (TURN_COMMANDS.include? commands[i])
-
-            move(commands[i]) if MOVE_COMMANDS.include? commands[i] 
-            turn(commands[i]) if TURN_COMMANDS.include? commands[i]  
-        end
+  def move(command)
+    case @direction
+    when 'N'
+      @position.y += 1 if command == 'f'
+      @position.y -= 1 if command == 'b'
+    when 'S'
+      @position.y -= 1 if command == 'f'
+      @position.y += 1 if command == 'b'
+    when 'W'
+      @position.x -= 1 if command == 'f'
+      @position.x += 1 if command == 'b'
+    when 'E'
+      @position.x += 1 if command == 'f'
+      @position.x -= 1 if command == 'b'
     end
+  end
 
-    def move(command)
-        case @direction
-        when 'N'
-            @position.y += 1 if command == 'f'
-            @position.y -= 1 if command == 'b'
-        when 'S'
-            @position.y -= 1 if command == 'f'
-            @position.y += 1 if command == 'b'
-        when 'W'
-            @position.x -= 1 if command == 'f'
-            @position.x += 1 if command == 'b'
-        when 'E'
-            @position.x += 1 if command == 'f'
-            @position.x -= 1 if command == 'b'
-        end
+  def turn(command)
+    case command
+    when 'l'
+      @direction = COORDINATES[(COORDINATES.index(@direction) - 1) % 4]
+    when 'r'
+      @direction = COORDINATES[(COORDINATES.index(@direction) + 1) % 4]
     end
-
-    def turn(command)
-        case @direction
-        when 'N'
-            @direction = 'W' if command == 'l'
-            @direction = 'E' if command == 'r'
-        when 'S'
-            @direction = 'E' if command == 'l'
-            @direction = 'W' if command == 'r'
-        when 'W'
-            @direction = 'S' if command == 'l'
-            @direction = 'N' if command == 'r'
-        when 'E'
-            @direction = 'N' if command == 'l'
-            @direction = 'S' if command == 'r'
-        end
-    end
+  end
 end
