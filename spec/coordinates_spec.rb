2cec2ae0d0d6d6cb3eb71908326c1deb026bc446
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-18 09:29:30 +0100

    Corrected the description.

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Coordinates do
     it 'is transitive'
     specify 'two coordinates with the same x- and y-coordinates are equal'
     specify 'two coordinates with the same x-coordinates but different y are unequal'
-    specify 'two coordinates with the same y-coordinates but equal y are unequal'
+    specify 'two coordinates with the same y-coordinates but equal x are unequal'
     ['(1, 1)', [1, 2], {x: 1, y: 2}, 1].each do |object_of_different_type|
       specify "coordinates are not equal to an object of a different type e.g. #{object_of_different_type}"
     end
