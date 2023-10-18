d470a790e151546cb92ee51f80e444ce7f40d597
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-18 09:47:47 +0100

    Test passes

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe Coordinates do
 
       expect(coordinate).not_to eq(coordinate_with_different_x)
     end
+
     ['(1, 1)', [1, 2], {x: 1, y: 2}, 1].each do |object_of_different_type|
       specify "coordinates are not equal to an object of a different type e.g. #{object_of_different_type}"
     end
