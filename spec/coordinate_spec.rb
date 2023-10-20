3c2af7228ffbe928bb3c448f2da9e4ce1e2cabc9
sblouet <stephen.blouet@simplybusiness.co.uk>
2023-10-20 15:39:34 +0100

    refactor: added context to coordinate spec

--- spec/coordinate_spec.rb
+++ spec/coordinate_spec.rb
@@ -1,14 +1,16 @@
 require_relative '../lib/coordinate.rb'
 
-describe Coordinate do
-    describe '#init' do
-        it 'has an x coordinate of 0 and a y coordinate of 0 when initialised without any parameters' do
-            coordinate = Coordinate.new(x_pos: 0, y_pos: 0)
+describe "Coordinate" do
+    context "when initialised without parameters" do
+        it 'has an x coordinate of 0 and a y coordinate of 0' do
+            coordinate = Coordinate.new
             expect(coordinate.x).to eq(0)
             expect(coordinate.y).to eq(0)
         end
+    end
 
-        it 'has an x coordinate and a y coordinate the same as that it was initialised with' do
+    context "when initialised with valid parameters" do
+        it 'has x and y coordinates equal to those it was initialised with' do
             coordinate = Coordinate.new(x_pos: 3, y_pos: 4)
             expect(coordinate.x).to eq(3)
             expect(coordinate.y).to eq(4)
