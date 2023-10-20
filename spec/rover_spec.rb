40b3f711793e02ce71ee570d3e1d1fc26f537a97
Nicola <ferrinicola86@gmail.com>
2023-10-20 14:47:28 +0100

    Use new implementation of coordinates and delete old one

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -5,7 +5,8 @@ describe Rover do
   describe '#init' do
     it 'has an initial position of 0,0 when not given an explicit start point' do
       rover = Rover.new
-      expect(rover.position).to eq([0,0])
+      expect(rover.coordinate_position.x).to eq(0)
+      expect(rover.coordinate_position.y).to eq(0)
     end
 
     it 'has position matching the coordinates it was initialized with' do
@@ -27,14 +28,14 @@ describe Rover do
 
   describe '#move' do
     it 'does not move if it receives an empty route' do
-      rover = Rover.new(coordinates:[3,4])
+      rover = Rover.new(coordinates: Coordinate.new(3,4))
       rover.move([])
-      expect(rover.position[0]).to eq(3)
-      expect(rover.position[1]).to eq(4)
+      expect(rover.coordinate_position.x).to eq(3)
+      expect(rover.coordinate_position.y).to eq(4)
     end
 
     it 'does raise an argument error exception if it receive an invalid route' do
-      rover = Rover.new(coordinates:[3,4])
+      rover = Rover.new(coordinates: Coordinate.new(3,4))
       expect {rover.move(['x','y'])}.to raise_error "ArgumentError"
     end
 
@@ -54,7 +55,7 @@ describe Rover do
     directions_mapping_right.each do |start_direction,expected_direction|
       # puts directions
       it "does face #{expected_direction} when receives a single right command in the route and starting direction of #{start_direction}" do
-        rover = Rover.new(direction: start_direction, coordinates:[3,4])
+        rover = Rover.new(direction: start_direction, coordinates: Coordinate.new(3,4))
         rover.move(['r'])
         expect(rover.direction).to eq(expected_direction)
       end
@@ -63,7 +64,7 @@ describe Rover do
     directions_mapping_left.each do |start_direction,expected_direction|
       # puts directions
       it "does face #{expected_direction} when receives a single left command in the route and starting direction of #{start_direction}" do
-        rover = Rover.new(direction: start_direction, coordinates:[3,4])
+        rover = Rover.new(direction: start_direction, coordinates: Coordinate.new(3,4))
         rover.move(['l'])
         expect(rover.direction).to eq(expected_direction)
       end
@@ -78,42 +79,44 @@ describe Rover do
     directions_mapping_3x_right.each do |start_direction,expected_direction|
       # puts directions
       it "does face #{expected_direction} when receives a three right commands in the route and starting direction of #{start_direction}" do
-        rover = Rover.new(direction: start_direction, coordinates:[3,4])
+        rover = Rover.new(direction: start_direction, coordinates: Coordinate.new(3,4))
         rover.move(['r','r','r'])
         expect(rover.direction).to eq(expected_direction)
       end
     end
 
     it "does face North when receives a two right and two left commands in the route and starting direction of North" do
-      rover = Rover.new(direction: 'N', coordinates:[3,4])
+      rover = Rover.new(direction: 'N', coordinates: Coordinate.new(3,4))
       rover.move(['r','r','l','l'])
       expect(rover.direction).to eq('N')
     end
 
-    directions_mapping_forward = { 'N' => [3,5],
-    'S' => [3,3],
-    'W' => [2,4],
-    'E' => [4,4]
+    directions_mapping_forward = { 'N' => Coordinate.new(3,5),
+    'S' => Coordinate.new(3,3),
+    'W' => Coordinate.new(2,4),
+    'E' => Coordinate.new(4,4)
     }
 
     directions_mapping_forward.each do |start_direction,expected_position|
       it "does move forward when receives a single forward command with direction #{start_direction}" do
-        rover = Rover.new(direction: start_direction, coordinates:[3,4])
+        rover = Rover.new(direction: start_direction, coordinates: Coordinate.new(3,4))
         rover.move(['f'])
-        expect(rover.position).to eq(expected_position)
+        expect(rover.coordinate_position.x).to eq(expected_position.x)
+        expect(rover.coordinate_position.y).to eq(expected_position.y)
       end
     end
 
-    directions_mapping_backward = { 'N' => [3,3],
-      'S' => [3,5],
-      'W' => [4,4],
-      'E' => [2,4]
+    directions_mapping_backward = { 'N' => Coordinate.new(3,3),
+      'S' => Coordinate.new(3,5),
+      'W' => Coordinate.new(4,4),
+      'E' => Coordinate.new(2,4)
     }
     directions_mapping_backward.each do |start_direction,expected_position|
       it "does move backward when receives a single backward command with direction #{start_direction}" do
-        rover = Rover.new(direction: start_direction, coordinates:[3,4])
+        rover = Rover.new(direction: start_direction, coordinates: Coordinate.new(3,4))
         rover.move(['b'])
-        expect(rover.position).to eq(expected_position)
+        expect(rover.coordinate_position.x).to eq(expected_position.x)
+        expect(rover.coordinate_position.y).to eq(expected_position.y)
       end
     end
 
