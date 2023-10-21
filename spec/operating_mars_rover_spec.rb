08f39413004cf3e6c4c9db8289d3d67ab8528fe9
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-21 18:28:32 +0100

    Parallel change: migrated to the new assertion.

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -173,6 +173,7 @@ RSpec.describe 'Operating a Mars Rover' do
       mars_rover.execute(['l'])
 
       expect(mars_rover.direction).to eq('N')
+      expect(mars_rover).to be_facing('N')
     end
 
     example "a Mars rover facing S turns left to face E" do
