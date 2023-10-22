6e4777e1f65586686782c244d1d7efcac40f7e03
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-22 11:59:35 +0100

    Refactor: Move Method using Parallel Change. We copy the method to Coordinates.

--- lib/coordinates.rb
+++ lib/coordinates.rb
@@ -6,6 +6,19 @@ class Coordinates
     @y = y
   end
 
+  def backwards(facing_direction)
+    case facing_direction
+    when 'N'
+      Coordinates.new(x: x, y: y - 1)
+    when 'E'
+      Coordinates.new(x: x - 1, y: y)
+    when 'S'
+      Coordinates.new(x: x, y: y + 1)
+    when 'W'
+      Coordinates.new(x: x + 1, y: y)
+    end
+  end
+
   def ==(other_coordinates)
     self.class == other_coordinates.class && y == other_coordinates.y && x == other_coordinates.x
   end
