f9ef5d03c13b7e3fb503999c1da56aa54086c9a5
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-24 11:55:27 +0100

    Refactor: Move method to what feels like a more sensible place (a Map).

--- lib/map.rb
+++ lib/map.rb
@@ -6,6 +6,23 @@ class Map
     @left_hand_edge = -10
   end
 
+  def move_forwards(current_position:, direction:)
+    case direction
+    when 'N'
+      Coordinates.new(x: current_position.x, y: current_position.y + 1)
+    when 'E'
+      if at_right_hand_edge?(current_position)
+        Coordinates.new(x: left_hand_edge, y: current_position.y)
+      else
+        Coordinates.new(x: current_position.x + 1, y: current_position.y)
+      end
+    when 'S'
+      Coordinates.new(x: current_position.x, y: current_position.y - 1)
+    when 'W'
+      Coordinates.new(x: current_position.x - 1, y: current_position.y)
+    end
+  end
+
   def at_right_hand_edge?(position)
     position.x == @right_hand_edge
   end
