fcfddcbfaa1e9b4d13e2becebde4337df1c797dd
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-25 11:16:00 +0100

    We will restrict ourselves to the positive quadrant.

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -10,21 +10,21 @@ RSpec.describe 'Operating a Mars Rover' do
   end
 
   it 'has a starting position anywhere away from an origin' do
-    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: -1), direction: 'N')
+    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: 1), direction: 'N')
 
-    expected_position = Coordinates.new(x: 1, y: -1)
+    expected_position = Coordinates.new(x: 1, y: 1)
     expect(mars_rover).to be_located_at(expected_position)
   end
 
   it 'has a direction' do
-    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: -1), direction: 'S')
+    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: 2), direction: 'S')
 
     expect(mars_rover).to be_facing('S')
   end
 
   %w{N E S W}.each do |direction|
     it "can face any direction, e.g. #{direction}" do
-      mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: -1), direction: direction)
+      mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 2, y: 0), direction: direction)
 
       expect(mars_rover).to be_facing(direction)
     end
@@ -33,7 +33,7 @@ RSpec.describe 'Operating a Mars Rover' do
   ['Unsupported', 'A', 1, {direction: 'north'}].each do |direction|
     it "cannot be operated when it has a direction that is not north, east, south or west e.g. '#{direction}'" do
       pending 'Discussion with the customer'
-      expect { MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: -1), direction: direction) }
+      expect { MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: 0), direction: direction) }
         .to raise_error('Direction must be one of N, E, S or W')
     end
 
@@ -48,7 +48,7 @@ RSpec.describe 'Operating a Mars Rover' do
   end
 
   it 'receives a list of commands' do
-    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: -1, y: -1), direction: 'S')
+    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 0), direction: 'S')
 
     expect(mars_rover).to respond_to(:execute).with(1)
   end
@@ -78,12 +78,12 @@ RSpec.describe 'Operating a Mars Rover' do
         expected_position: Coordinates.new(x: 1, y: 0)
       },
       {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 0), direction: 'S'),
-        expected_position: Coordinates.new(x: 0, y: -1)
+        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 1), direction: 'S'),
+        expected_position: Coordinates.new(x: 0, y: 0)
       },
       {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 0), direction: 'W'),
-        expected_position: Coordinates.new(x: -1, y: 0)
+        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: 0), direction: 'W'),
+        expected_position: Coordinates.new(x: 0, y: 0)
       }
     ].each do |row|
       example "#{row[:mars_rover].inspect} moves forwards to #{row[:expected_position]}" do
@@ -184,8 +184,8 @@ RSpec.describe 'Operating a Mars Rover' do
   describe 'Moving backwards' do
     [
       {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 0), direction: 'N'),
-        expected_position: Coordinates.new(x: 0, y: -1)
+        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 1), direction: 'N'),
+        expected_position: Coordinates.new(x: 0, y: 0)
       },
       {
         mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 2), direction: 'N'),
@@ -226,7 +226,7 @@ RSpec.describe 'Operating a Mars Rover' do
 
     %w{N E S W}.each do |direction|
       it "never changes direction e.g. it remains facing #{direction}" do
-        anywhere = Coordinates.new(x: -1, y: 2)
+        anywhere = Coordinates.new(x: 0, y: 2)
         mars_rover = MarsRover.new(map: Map.new, starting_position: anywhere, direction: direction)
 
         expect { mars_rover.execute(['b']) }.not_to change(mars_rover, :direction)
@@ -321,14 +321,14 @@ RSpec.describe 'Operating a Mars Rover' do
     end
 
     it 'remains at its current position' do
-      mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: -3, y: -4), direction: 'E')
+      mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 4), direction: 'E')
 
       expect { mars_rover.execute(['r']) }.not_to change(mars_rover, :current_position)
     end
   end
 
   it 'does not execute any commands it does not recognise' do
-    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: -1, y: 5), direction: 'W')
+    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: 5), direction: 'W')
 
     expect { mars_rover.execute(['x']) }.not_to change(mars_rover, :direction)
     expect { mars_rover.execute(['z']) }.not_to change(mars_rover, :current_position)
