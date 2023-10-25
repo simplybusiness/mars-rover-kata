6eabea0bb10a3b544f8795be9083e85d3911b5cc
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-25 13:01:41 +0100

    Refactor: Moved the tests to a script for the feature.

---
+++ spec/moving_backwards_spec.rb
@@ -0,0 +1,65 @@
+require_relative '../lib/coordinates'
+require_relative '../lib/map'
+require_relative '../lib/mars_rover'
+describe 'Moving backwards' do
+  [
+    {
+      mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 1), direction: 'N'),
+      expected_position: Coordinates.new(x: 0, y: 0)
+    },
+    {
+      mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 2), direction: 'N'),
+      expected_position: Coordinates.new(x: 0, y: 1)
+    },
+    {
+      mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 2, y: 3), direction: 'N'),
+      expected_position: Coordinates.new(x: 2, y: 2)
+    },
+    {
+      mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 2, y: 0), direction: 'E'),
+      expected_position: Coordinates.new(x: 1, y: 0)
+    },
+    {
+      mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 0), direction: 'S'),
+      expected_position: Coordinates.new(x: 0, y: 1)
+    },
+    {
+      mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 0), direction: 'W'),
+      expected_position: Coordinates.new(x: 1, y: 0)
+    }
+  ].each do |row|
+    example "#{row[:mars_rover].inspect} moves backwards to #{row[:expected_position]}" do
+      mars_rover = row[:mars_rover]
+
+      mars_rover.execute(['b'])
+
+      expect(mars_rover).to be_located_at(row[:expected_position])
+    end
+  end
+
+  it 'can move backwards multiple times' do
+    mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 3, y: 1), direction: 'E')
+
+    expected_location = Coordinates.new(x: 0, y: 1)
+    expect { mars_rover.execute(%w{b b b}) }.to change(mars_rover, :current_position).to expected_location
+  end
+
+  %w{N E S W}.each do |direction|
+    it "never changes direction e.g. it remains facing #{direction}" do
+      anywhere = Coordinates.new(x: 0, y: 2)
+      mars_rover = MarsRover.new(map: Map.new, starting_position: anywhere, direction: direction)
+
+      expect { mars_rover.execute(['b']) }.not_to change(mars_rover, :direction)
+    end
+  end
+
+  private
+
+  RSpec::Matchers.define(:be_located_at) do |expected_position|
+    match { |mars_rover| mars_rover.current_position == expected_position }
+  end
+
+  RSpec::Matchers.define(:be_facing) do |expected_direction|
+    match { |mars_rover| mars_rover.direction == expected_direction }
+  end
+end
