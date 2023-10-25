49a32f442d95285451478f263da2ccfaaecc23fc
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-25 13:07:02 +0100

    Removed some duplicate tests.

--- spec/operating_mars_rover_spec.rb
+++ spec/operating_mars_rover_spec.rb
@@ -60,59 +60,6 @@ RSpec.describe 'Operating a Mars Rover' do
       .to raise_error('A command must consist of a list of individual commands e.g. %w{f b l r}')
   end
 
-  describe 'Moving backwards' do
-    [
-      {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 1), direction: 'N'),
-        expected_position: Coordinates.new(x: 0, y: 0)
-      },
-      {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 2), direction: 'N'),
-        expected_position: Coordinates.new(x: 0, y: 1)
-      },
-      {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 2, y: 3), direction: 'N'),
-        expected_position: Coordinates.new(x: 2, y: 2)
-      },
-      {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 2, y: 0), direction: 'E'),
-        expected_position: Coordinates.new(x: 1, y: 0)
-      },
-      {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 0), direction: 'S'),
-        expected_position: Coordinates.new(x: 0, y: 1)
-      },
-      {
-        mars_rover: MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 0, y: 0), direction: 'W'),
-        expected_position: Coordinates.new(x: 1, y: 0)
-      }
-    ].each do |row|
-      example "#{row[:mars_rover].inspect} moves backwards to #{row[:expected_position]}" do
-        mars_rover = row[:mars_rover]
-
-        mars_rover.execute(['b'])
-
-        expect(mars_rover).to be_located_at(row[:expected_position])
-      end
-    end
-
-    it 'can move backwards multiple times' do
-      mars_rover = MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 3, y: 1), direction: 'E')
-
-      expected_location = Coordinates.new(x: 0, y: 1)
-      expect { mars_rover.execute(%w{b b b}) }.to change(mars_rover, :current_position).to expected_location
-    end
-
-    %w{N E S W}.each do |direction|
-      it "never changes direction e.g. it remains facing #{direction}" do
-        anywhere = Coordinates.new(x: 0, y: 2)
-        mars_rover = MarsRover.new(map: Map.new, starting_position: anywhere, direction: direction)
-
-        expect { mars_rover.execute(['b']) }.not_to change(mars_rover, :direction)
-      end
-    end
-  end
-
   describe 'Turning left' do
     def self.mars_rover_facing(direction:)
       MarsRover.new(map: Map.new, starting_position: Coordinates.new(x: 1, y: 2), direction: direction)
