6c0db225048a8dfc28a445c1da57e415edfc8c4a
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-30 16:30:04 +0000

    Moved the logic into the forwards method.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -13,12 +13,7 @@ class MarsRover
         when 'b'
           backwards(location)
         when 'f'
-          new_location = forwards(location)
-          if @map.located_at_north_pole? new_location.coordinates
-            Location.new(coordinates: Coordinates.new(x: new_location.coordinates.x + 18, y: 8), direction: 'S')
-          else
-            new_location
-          end
+          forwards(location)
         when 'l'
           location.rotate_left
         when 'r'
@@ -44,7 +39,12 @@ class MarsRover
   private
 
   def forwards(current_location)
-    @map.next_location_forwards(location: current_location)
+    new_location = @map.next_location_forwards(location: current_location)
+    if @map.located_at_north_pole? new_location.coordinates
+      Location.new(coordinates: Coordinates.new(x: new_location.coordinates.x + 18, y: 8), direction: 'S')
+    else
+      new_location
+    end
   end
 
   def backwards(current_location)
