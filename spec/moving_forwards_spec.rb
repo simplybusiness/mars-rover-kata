bdae745468fcb3d486e89a1884f864509355a69a
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-30 17:48:56 +0000

    Pushed a detail up the callstack.

--- spec/moving_forwards_spec.rb
+++ spec/moving_forwards_spec.rb
@@ -160,7 +160,7 @@ describe 'Moving forwards' do
 
   private
 
-  def a_mars_rover(located_at:)
-    MarsRover.new(map: Map.new(x_domain: (0..10), y_domain: (-9..9)), starting_location: located_at)
+  def a_mars_rover(map: Map.new(x_domain: (0..10), y_domain: (-9..9)), located_at:)
+    MarsRover.new(map: map, starting_location: located_at)
   end
 end
