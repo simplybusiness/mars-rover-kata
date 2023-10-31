7ede2024280c0f96fe0e42ec98a13d6f08fc5366
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-31 18:55:06 +0000

    Refactor: moved some logic to a more sensible place.

--- lib/location.rb
+++ lib/location.rb
@@ -28,6 +28,11 @@ class Location
         coordinates: Coordinates.new(x: coordinates.x, y: (coordinates.y + 1)),
         direction: direction
       )
+    when 'E'
+      Location.new(
+        coordinates: Coordinates.new(x: coordinates.x + 1, y: coordinates.y),
+        direction: direction
+      )
     when 'S'
       Location.new(
         coordinates: Coordinates.new(x: coordinates.x, y: (coordinates.y - 1)),
