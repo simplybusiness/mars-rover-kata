01c1a5dfb32244e73d58ac497d4d70d6e38fccc9
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-31 18:34:52 +0000

    Changed the input of the test to allow for the removal of a duplicate method.

--- spec/moving_forwards_spec.rb
+++ spec/moving_forwards_spec.rb
@@ -1,6 +1,6 @@
 require 'mars_rover_helper'
 describe 'Moving forwards' do
-  def self.mars_rover(map: Map.new(x_domain: (0..10), y_domain: (0..10)), located_at:)
+  def self.mars_rover(map: Map.new(x_domain: (0..10), y_domain: (-9..9)), located_at:)
     MarsRover.new(map: map, starting_location: located_at)
   end
   private_class_method :mars_rover
