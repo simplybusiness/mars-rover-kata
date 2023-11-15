995609533bf5c24af6d7728d7c181bdf281e35b4
Melissa Teng <melissa.teng@simplybusiness.co.uk>
2023-11-15 12:55:47 +0000

    create class and initial test

---
+++ spec/mars_rover_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/mars_rover"
+
+describe MarsRover do
+    describe "initialisation" do
+        it "default starting point of x = 0 and y = 0" do
+        end
+
+        it "starting point of x and y given by the starting position" do
+        end
+    end
+end
