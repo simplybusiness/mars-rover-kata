1e175bf6a08a34c23986b7ab29f6e350ecaa24fa
Melissa Teng <melissa.teng@simplybusiness.co.uk>
2023-11-15 14:36:57 +0000

    add initialisation test

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,7 +1,11 @@
+require_relative "spec_helper"
 require_relative "../lib/mars_rover"
 
 describe MarsRover do
     describe "initialisation" do
+        it "raise Argument error if starting point is not given upon initialisation" do
+        end
+
         it "default starting point of x = 0 and y = 0" do
         end
 
