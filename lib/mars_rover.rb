698d6c1003f3efe9b7706d205ecc927f89dc9bdf
Melissa Teng <melissa.teng@simplybusiness.co.uk>
2023-11-28 22:50:17 +0800

    refactor: add coordinates variable via parallel change

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,15 +1,21 @@
 class MarsRover
     attr_reader :position
     attr_reader :direction
+    attr_reader :coordinates
    
     def initialize(x, y, direction, coordinates = [0, 0])
         directions = ["N", "S", "W", "E"]
 
         raise ArgumentError unless x.is_a?(Integer) && y.is_a?(Integer)
         raise ArgumentError unless x >= 0 && y >= 0
+
+        raise ArgumentError unless coordinates[0].is_a?(Integer) && coordinates[1].is_a?(Integer)
+        raise ArgumentError unless coordinates[0] >= 0 && coordinates[1] >= 0
+
         raise ArgumentError unless directions.include?(direction)
 
         @position = [x, y]
+        @coordinates = coordinates
         @direction = direction
     end
     
