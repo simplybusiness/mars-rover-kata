82f6a67624da3749678b2f6a2ba83a9a41e9cbf3
Melissa Teng <melissa.teng@simplybusiness.co.uk>
2024-01-18 13:36:31 +0000

    test: implement test to compare coordinates with different x and y

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -19,7 +19,12 @@ describe Coordinates do
       different_x_coordinates = Coordinates.new(x: 2, y: 2)
       expect(coordinates).not_to eq(different_x_coordinates)
     end
-    it 'returns false if compared with a Coordinates object with different x and y'
+
+    it 'returns false if compared with a Coordinates object with different x and y' do
+      coordinates = Coordinates.new(x: 1, y: 1)
+      different_coordinates = Coordinates.new(x: 2, y: 2)
+      expect(coordinates).not_to eq(different_coordinates)
+    end
     it 'returns false if compared with anything that is not a Coordinates object'
   end
 end
