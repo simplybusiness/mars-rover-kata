c2c59c0ad4d23b31b1edd04cf39f89a5d513266a
Jd Soteldo <juan-diego.soteldo@simplybusiness.com>
2024-02-12 11:26:15 -0600

    Initialized direction

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,8 +1,9 @@
 class MarsRover
-  attr_accessor :x, :y
+  attr_accessor :x, :y, :direction
 
-  def initialize(x: 0, y: 0)
+  def initialize(x: 0, y: 0, direction: 'N')
     @x = x
     @y = y
+    @direction = direction
   end
 end
