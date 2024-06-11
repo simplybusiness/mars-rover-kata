3ff29e8857fee5893fa0f0024f7d95ada07b4bb8
Anija Thomas <anija.thomas@simplybusiness.co.uk>
2024-06-11 16:56:17 +0100

    another spec to raise error when coordinates are unknown

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -8,7 +8,11 @@ RSpec.describe 'Rover' do
     end
 
     it 'knows the direction'
-    it 'errors when does not know the coordinates'
+
+    it 'errors when does not know the coordinates' do
+      expect{ Rover.new().initial_points }.to raise_error
+    end
+
     it 'errors when direction is unavailable'
     it 'errors when the direction is not one of the cardinal directions'
   end
