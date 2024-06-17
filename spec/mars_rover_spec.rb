fe33281ca020b097936086235e2946169f1e9ff1
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-06-17 13:57:18 +0100

    feature: we can get the current position of the Rover from anywhere.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,15 +1,22 @@
 describe 'Mars Rover' do
   class MarsRover
-    def current_position
-      [1, 2]
+    def current_position(coordinates)
+      coordinates
     end
   end
 
   it 'has an initial position' do
     mars_rover = MarsRover.new
 
-    expect(mars_rover.current_position).to eq([1, 2])
+    expect(mars_rover.current_position([1, 2])).to eq([1, 2])
   end
+
+  it 'has an initial position at any point on the planet' do
+    mars_rover = MarsRover.new
+
+    expect(mars_rover.current_position([0, 0])).to eq([0, 0])
+  end
+
   it 'has a direction that it is facing'
   it 'cannot have a direction outside of N, E, S or W'
   it 'cannot have a null position'
