afb916db11e81f11de026323432a0edd2ae5d19f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-06-17 13:30:44 +0100

    test: added some edge cases.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,4 +1,6 @@
 describe 'Mars Rover' do
   it 'has an initial position'
   it 'has a direction that it is facing'
+  it 'cannot have a direction outside of N, E, S or W'
+  it 'cannot have a null position'
 end
