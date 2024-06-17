9b147bbd8a07fc13b7f8e073a0bdb0c047028035
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-06-17 13:40:44 +0100

    feat: we can get the initial position of a Mars Rover provide it is (1, 2).

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,5 +1,15 @@
 describe 'Mars Rover' do
-  it 'has an initial position'
+  class MarsRover
+    def current_position
+      [1, 2]
+    end
+  end
+
+  it 'has an initial position' do
+    mars_rover = MarsRover.new
+
+    expect(mars_rover.current_position).to eq([1, 2])
+  end
   it 'has a direction that it is facing'
   it 'cannot have a direction outside of N, E, S or W'
   it 'cannot have a null position'
