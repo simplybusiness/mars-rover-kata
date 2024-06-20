afe6f493791dd011c7c23374fcfbe1bfe4b755e5
Shaun Bokenham <shaun.bokenham@simplybusiness.co.uk>
2024-06-20 10:58:54 +0100

    Fix camelCase to snake_case for method name

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -2,8 +2,8 @@ class MarsRover
   attr_reader :direction
 
   def initialize(x, y, direction)
-    setPosition(x, y)
-    setDirection direction
+    set_position(x, y)
+    set_direction direction
   end
 
   def position
@@ -12,16 +12,18 @@ class MarsRover
 
   private
 
-  def setPosition(x, y)
+  def set_position(x, y)
     unless x.is_a?(Integer) && y.is_a?(Integer)
       raise ArgumentError, 'Invalid x or y position, both must be an integer'
     end
+
+    
 
     @x = x
     @y = y
   end
 
-  def setDirection(direction)
+  def set_direction(direction)
     unless ['north', 'south', 'east', 'west'].include? direction
       ## TODO change to argument error
       raise Exception.new, "Invalid direction: '#{direction}'"
