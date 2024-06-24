99179fb28a2c13059a039f590b262d449d894c5c
Litty John <litty.john@simplybusiness.co.uk>
2024-06-24 13:40:29 +0100

    test for the current direction

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -6,6 +6,9 @@ describe 'Mars Rover' do
     def current_position
       @current_position
     end
+    def current_direction
+      'N'
+    end
   end
 
   it 'has an initial position' do
@@ -13,7 +16,10 @@ describe 'Mars Rover' do
     expect(mars_rover.current_position).to eq([1, 2])
   end
 
-  it 'has a direction that it is facing'
+  it 'has a direction that it is facing' do
+    mars_rover = MarsRover.new([1, 2])
+    expect(mars_rover.current_direction).to eq('N')
+  end
   it 'cannot have a direction outside of N, E, S or W'
   it 'cannot have a null position'
 end
