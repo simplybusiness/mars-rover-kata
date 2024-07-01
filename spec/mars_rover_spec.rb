985898fd5c2f895d50718a266efa9c417a167b60
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-07-01 10:47:40 +0100

    test: added a test list for the first requirement.

---
+++ spec/mars_rover_spec.rb
@@ -0,0 +1,6 @@
+describe 'Mars Rover' do
+  it 'knows its starting point'
+  it 'knows the direction is facing (N, S, E, W)'
+  it 'cannot be initialized with a direction outside of (N, S, E, W)'
+  it 'cannot be initialized w/o a starting point'
+end
