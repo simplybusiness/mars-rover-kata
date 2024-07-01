dd2ab79aa45b7f07beb6bb975e097c3b0e75abdc
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-07-01 10:58:51 +0100

    feat: we can query the Mars Rover's current position correctly as long as it starts at the origin.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,5 +1,24 @@
+require 'ostruct'
 describe 'Mars Rover' do
-  it 'knows its starting point'
+  class MarsRover
+    def initialize(starting_point:)
+
+    end
+
+    def current_position
+      OpenStruct.new(x: 0, y: 0)
+    end
+
+    def inspect
+      'a Mars Rover'
+    end
+  end
+
+  it 'knows its starting point' do
+    mars_rover = MarsRover.new(starting_point: OpenStruct.new(x: 0, y: 0))
+
+    expect(mars_rover.current_position).to eq(OpenStruct.new(x: 0, y: 0))
+  end
   it 'knows the direction is facing (N, S, E, W)'
   it 'cannot be initialized with a direction outside of (N, S, E, W)'
   it 'cannot be initialized w/o a starting point'
