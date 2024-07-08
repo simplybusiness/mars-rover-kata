8aab27389c3fc1458cf1ce8c89b6f92e9f3523d3
niki namrata <niki.namrata@simplybusiness.co.uk>
2024-07-08 11:20:49 +0100

    added step definitions for the mars_rover feature test

---
+++ features/mars_rover_position_steps.rb
@@ -0,0 +1,8 @@
+
+Given('The Mars Rover is at \({int}, {int}) starting position') do |int, int2|
+   pending 
+  end
+
+When('we ask its current position') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
