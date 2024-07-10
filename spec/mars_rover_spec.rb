20add82b5a7918380c989cdc9a0bddc3c8f53a6b
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2024-07-10 13:44:22 +0100

    add describe and context blocks

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,17 +2,24 @@ require 'ostruct'
 require './mars_rover/mars_rover.rb'
 
 describe 'Mars Rover' do
-  it 'knows its starting point' do
-    mars_rover = MarsRover.new(starting_point: OpenStruct.new(x: 0, y: 0))
 
-    expect(mars_rover.current_position).to eq(OpenStruct.new(x: 0, y: 0))
+  describe 'initialization of the mars rover' do
+    context 'when the starting point is provided' do
+      it 'knows its starting point' do
+        mars_rover = MarsRover.new(starting_point: OpenStruct.new(x: 0, y: 0))
+
+        expect(mars_rover.current_position).to eq(OpenStruct.new(x: 0, y: 0))
+      end
+
+      it 'has a starting point anywhere on the planet' do
+        mars_rover = MarsRover.new(starting_point: OpenStruct.new(x: 2, y: 3))
+
+        expect(mars_rover.current_position).to eq(OpenStruct.new(x: 2, y: 3))
+      end
+    end
   end
 
-  it 'has a starting point anywhere on the planet' do
-    mars_rover = MarsRover.new(starting_point: OpenStruct.new(x: 2, y: 3))
 
-    expect(mars_rover.current_position).to eq(OpenStruct.new(x: 2, y: 3))
-  end
 
   it 'knows the direction is facing (N, S, E, W)' do
     mars_rover = MarsRover.new(starting_point: OpenStruct.new(x: 2, y: 3), direction: 'N')
