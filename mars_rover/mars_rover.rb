28d8f7f853bc318b171dda033d6201a5edda629d
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2024-07-10 13:38:54 +0100

    remove openstruct from client

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 class MarsRover
   attr_reader :current_position, :direction
 
-  def initialize(starting_point: OpenStruct.new(x: 0, y: 0), direction: 'N')
+  def initialize(starting_point:, direction: 'N')
     @current_position = starting_point
     raise ArgumentError, "Direction must be one of 'N', 'E', 'S', 'W'" unless %w[N E S W].include?(direction)
     @direction = direction
