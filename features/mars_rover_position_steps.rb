c8e5e5cf7ee4f57524aabb0400d8bf34526521f5
niki namrata <niki.namrata@simplybusiness.co.uk>
2024-07-22 10:57:05 +0100

    added class

--- features/mars_rover_position_steps.rb
+++ features/mars_rover_position_steps.rb
@@ -5,4 +5,12 @@ Given('The Mars Rover is at \({int}, {int}) starting position') do |int, int2|
 
 When('we ask its current position') do
     pending # Write code here that turns the phrase above into concrete actions
+
+  Then('we get a signal back that its starting position is \({int}, {int})') do |int, int2|
+      pending # Write code here that turns the phrase above into concrete actions
+    end
+    
+    Then('we also get a signal back about its current direction') do
+      pending # Write code here that turns the phrase above into concrete actions
+    end
   end
