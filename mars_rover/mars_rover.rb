65ab4141ba248af42cb5040da323bcc38621f031
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2024-07-24 13:46:01 +0100

    refactor and add validate direction

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -5,11 +5,15 @@ class MarsRover
 
   def initialize(starting_point:, direction: 'N')
     @current_position = starting_point
-    @direction = (direction || 'N').upcase
+    @direction = validate_direction(direction)
+  end
 
-    unless VALID_DIRECTIONS.include?(@direction)
+  def validate_direction(direction)
+    direction = (direction || 'N').upcase
+    unless VALID_DIRECTIONS.include?(direction)
       raise ArgumentError, "Direction must be one of #{VALID_DIRECTIONS.join(', ')}"
     end
+    direction
   end
 
   def inspect
