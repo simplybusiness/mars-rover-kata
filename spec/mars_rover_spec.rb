d090082624918d526a4f809afcb653a16033d011
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-07-29 15:05:54 +0100

    refactor: check the input is valid and then assign.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -4,13 +4,14 @@ describe 'Mars Rover' do
     attr_reader :current_position, :current_direction
 
     def initialize(current_position, current_direction)
+      raise 'Invalid direction' unless ['N', 'E', 'S', 'W'].include?(current_direction)
+      raise 'Invalid position' if current_position.nil?
+      raise 'Invalid position' unless current_position.is_a?(Array)
+      raise 'Invalid position' unless current_position.all? { |coordinate| coordinate.is_a?(Integer) }
+
       @current_position = current_position
       @current_coordinates = OpenStruct.new(x: current_position[0], y: current_position[1])
       @current_direction = current_direction
-      raise 'Invalid direction' unless ['N', 'E', 'S', 'W'].include?(@current_direction)
-      raise 'Invalid position' if @current_position.nil?
-      raise 'Invalid position' unless @current_position.is_a?(Array)
-      raise 'Invalid position' unless @current_position.all? { |coordinate| coordinate.is_a?(Integer) }
     end
 
     def receive_command(commands)
