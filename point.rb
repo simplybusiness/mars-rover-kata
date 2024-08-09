8e7eb21acdd5a5ac1a145f98900d2a25a85cd7eb
Anija Thomas <anija.thomas@simplybusiness.co.uk>
2024-08-09 11:32:30 +0100

    introduce y_coordiante definition and spec

--- point.rb
+++ point.rb
@@ -1,7 +1,8 @@
 class Point
-  attr_reader :x_coordinate
+  attr_reader :x_coordinate, :y_coordinate
 
-  def initialize(x_coordinate)
+  def initialize(x_coordinate=0, y_coordinate=0)
     @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
   end
 end
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe 'Point' do
       expect(Point.new(1).x_coordinate).to eq(1)
     end
 
-    it "has a y co-ordinate"
+    it "has a y co-ordinate" do
+      expect(Point.new(0,1).y_coordinate).to eq(1)
+    end
     specify "two co-ordinates with the same x and y co-ordinates are the same"
     specify "two co-ordinates with different x or y co-ordinates are unequal"
   end
