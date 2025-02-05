905d6aa69a0499c0a2f6b0b172d0b566812c0f76
niki namrata <niki.namrata@simplybusiness.co.uk>
2025-02-05 11:36:35 +0000

    extracted and put in a method

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -19,19 +19,22 @@ class MarsRover
 
     def execute(commands)
       if commands.first == "f"
-        if @current_direction == "N"
-          @y = @y + 1
-        elsif @current_direction == "E"
-          @x = @x + 1
-        elsif @current_direction == "S"
-           @y= @y - 1
-        elsif @current_direction == "W"
-           @x = @x - 1
-          
-        end
+        move_forward
       end
-
       "#{@x},#{@y},#{@current_direction}"
     end
+
+    def move_forward
+      if @current_direction == "N"
+        @y = @y + 1
+      elsif @current_direction == "E"
+        @x = @x + 1
+      elsif @current_direction == "S"
+         @y= @y - 1
+      elsif @current_direction == "W"
+         @x = @x - 1
+      end
+      
+    end  
   end 
   
