ef096e30b64652269d5b437e70a7a3f18480f99e
Pawel Dworzycki <pawel.dworzycki@simplybusiness.co.uk>
2025-03-03 13:59:17 +0000

    test: add happy path initialization test

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,5 +1,12 @@
+require_relative '../lib/rover'
+
 RSpec.describe 'Rover' do
-  it 'should initialize with a position and direction'
+  it 'should initialize with a position and direction' do
+    rover = Rover.new([1, 2], 'N')
+    expect(rover.position).to eq([1, 2])
+    expect(rover.direction).to eq('N')
+  end
+
   it 'should throw an error if initialised with an invalid position'
   it 'should throw an error if initialised with an invalid direction'
 end
