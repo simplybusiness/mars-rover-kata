e057baf2eaf48c9e3306e10617721a9ebec95348
Pawel Dworzycki <pawel.dworzycki@simplybusiness.co.uk>
2025-03-03 13:42:23 +0000

    test: add initial pending tests

---
+++ spec/rover_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe 'Rover' do
+  it 'should initialize with a position and direction'
+  it 'should throw an error if initialised with an invalid position'
+  it 'should throw an error if initialised with an invalid direction'
+end
