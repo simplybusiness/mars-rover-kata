33e0996b674e700d24b2f5811224865c9826ba78
Ahad Rahman <ahad.rahman@simplybusiness.co.uk>
2025-05-21 10:44:36 +0100

    Refactor with tests

---
+++ mars_rover.rb
@@ -0,0 +1,41 @@
+class MarsRover
+  attr_reader :x, :y, :direction
+
+  GRID_WIDTH = 10
+  GRID_HEIGHT = 10
+
+  DIRECTIONS = ['N', 'E', 'S', 'W'] # Ordered for a right turn
+
+  MOVEMENT_VECTORS = {
+    'N' => { x: 0, y: 1},
+    'E' => { x: 1, y: 0},
+    'S' => { x: 0, y: -1},
+    'W' => {x: -1, y: 0},
+  }
+
+  def initialize(start_x, start_y, start_direction)
+    @grid_width = GRID_WIDTH
+    @grid_height = GRID_HEIGHT
+
+    # if start_x < 0 || start_x >= GRID_WIDTH
+    unless start_x.between?(0, GRID_WIDTH)
+      raise ArgumentError, "x-coordinate is outside the grid"
+    end
+
+    if start_y < 0 || start_y >= GRID_HEIGHT
+      raise ArgumentError, "y-coordinate is outside the grid"
+    end
+
+    unless DIRECTIONS.include?(start_direction)
+      raise ArgumentError, "Direction is invalid"
+    end
+
+    @x = start_x
+    @y = start_y
+    @direction = start_direction
+
+  end
+end
+
+
+
---
+++ spec/mars_rover_spec.rb
@@ -0,0 +1,67 @@
+#   +---+---+---+---+---+---+---+---+---+---+
+# 9 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 8 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 7 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 6 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 5 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 4 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 3 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 2 | . | X | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 1 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+# 0 | . | . | . | . | . | . | . | . | . | . |
+#   +---+---+---+---+---+---+---+---+---+---+
+#     0   1   2   3   4   5   6   7   8   9
+
+require_relative '../mars_rover'
+
+
+describe "Initialisation" do
+  it "creates a rover with the correct initial state" do
+    rover = MarsRover.new(1, 2, "N")
+
+    expect(rover.x).to be == 1
+    expect(rover.y).to be == 2
+    expect(rover.direction).to be == "N"
+  end
+
+  context "with invalid parameters" do
+    it "raises an error if the x coordinate is out of bounds" do
+      expect{ MarsRover.new(23, 5, "N") }
+        .to raise_error(ArgumentError, "x-coordinate is outside the grid")
+    end
+
+    it "raises an error if the y coordinate is out of bounds" do
+      expect{ MarsRover.new(3, 15, "N") }
+        .to raise_error(ArgumentError, "y-coordinate is outside the grid")
+    end
+
+    it "raises an error if the direction is invalid" do
+      expect{ MarsRover.new(3, 5, "X") }
+        .to raise_error(ArgumentError, "Direction is invalid")
+    end
+  end
+end
+
+describe "Executing commands" do
+  rover = MarsRover.new(1, 2, "N")
+
+  context "move Rover forward" do
+    it "moves forward" do
+      # result = rover.execute("f")
+
+      # expect result == true
+      # expect rover.x == 1
+      # expect rover.y == 3
+      # expect rover.direction == "N"
+    end
+  end
+end
