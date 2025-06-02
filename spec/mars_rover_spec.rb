85e76970846c95d18d7927851955bdb6c52ad83f
Ahad Rahman <ahad.rahman@simplybusiness.co.uk>
2025-06-02 14:01:13 +0100

    Tests to check moving forward in all direction in and out of bounds

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -56,7 +56,7 @@ describe "Executing commands" do
   let(:rover) { MarsRover.new(1, 2, "N")}
 
   context "move Rover forward" do
-    it "moves forward North and stays within the grid" do
+    it "moves forward in the North direction and stay within the grid" do
       mars_rover = rover.execute("f")
 
       expect(mars_rover.x).to be == 1
@@ -64,20 +64,55 @@ describe "Executing commands" do
       expect(mars_rover.direction).to be == "N"
     end
 
-    it "moves forward which would cause the rover to go out of bounds" do
+    it "moves forward in the North direction which would cause the rover to go out of bounds" do
       rover = MarsRover.new(2, 10, "N")
 
       expect { rover.execute("f") }.to raise_error(ArgumentError, "Rover will go out of bounds")
     end
 
-    it "moves forward in the East direction" do
+    it "moves forward in the East direction and stay within the grid" do
       rover = MarsRover.new(1, 2, "E")
       rover = rover.execute("f")
 
       expect(rover.x).to be == 2
       expect(rover.y).to be == 2
       expect(rover.direction).to be == "E"
-   
+    end
+
+    it "moves forward in the East direction which would cause the rover to go out of bounds" do
+      rover = MarsRover.new(10, 2, "E")
+
+      expect { rover.execute("f") }.to raise_error(ArgumentError, "Rover will go out of bounds")
+    end
+
+    it "moves forward in the West direction and stay within the grid" do
+      rover = MarsRover.new(1, 2, "W")
+      rover = rover.execute("f")
+
+      expect(rover.x).to be == 0
+      expect(rover.y).to be == 2
+      expect(rover.direction).to be == "W"
+    end
+
+    it "moves forward in the West direction which would cause the rover to go out of bounds" do
+      rover = MarsRover.new(0, 2, "W")
+
+      expect { rover.execute("f") }.to raise_error(ArgumentError, "Rover will go out of bounds")
+    end
+
+    it "moves forward in the South direction and stay within the grid" do
+      rover = MarsRover.new(1, 2, "S")
+      rover = rover.execute("f")
+
+      expect(rover.x).to be == 1
+      expect(rover.y).to be == 1
+      expect(rover.direction).to be == "S"
+    end
+
+    it "moves forward in the South direction which would cause the rover to go out of bounds" do
+      rover = MarsRover.new(1, 0, "S")
+
+      expect { rover.execute("f") }.to raise_error(ArgumentError, "Rover will go out of bounds")
     end
       
   end
