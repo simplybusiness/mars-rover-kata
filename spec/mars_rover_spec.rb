96cb73be72f17810fe1649d10fadd5a3c2c1f510
Grace Calianese <grace.calianese@simplybusiness.com>
2025-08-19 12:14:29 -0400

    first test

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,4 +1,12 @@
 RSpec.describe 'mars rover' do
-    it 'has a current position'
+    class MarsRover
+        def current_position
+            [0, 0]
+        end
+    end
+    it 'has a current position' do
+        mars_rover = MarsRover.new
+        expect(mars_rover.current_position).to eq [0, 0]
+    end
     it 'has a direction'
 end
