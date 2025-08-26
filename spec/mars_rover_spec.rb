71dd82a7a06ca7c5ff84890f1fc39bff8dfb861a
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-08-26 17:01:26 +0100

    refactor: push detail up the call stack.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -2,7 +2,8 @@ require_relative "../mars_rover"
 
 RSpec.describe 'mars rover' do
     it 'has a current position' do
-        mars_rover = MarsRover.new
+        mars_rover = MarsRover.new(starting_position: [0, 0])
+
         expect(mars_rover.current_position).to eq [0, 0]
     end
 
