e31bd872074001dcb41206656586814c467735d1
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-08-26 16:57:09 +0100

    feat: the Mars Rover can land anywhere on the planet next to our rock that's modeled as the origin.

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,6 +1,6 @@
 class MarsRover
-    def initialize
-        @current_position = [0, 0]
+    def initialize(starting_position: [0, 0])
+        @current_position = starting_position
     end
 
     attr_reader :current_position
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -5,5 +5,12 @@ RSpec.describe 'mars rover' do
         mars_rover = MarsRover.new
         expect(mars_rover.current_position).to eq [0, 0]
     end
+
+    it 'has a current position away from the origin' do
+      mars_rover = MarsRover.new(starting_position: [0, 1])
+
+      expect(mars_rover.current_position).to eq [0, 1]
+    end
+
     it 'has a direction'
 end
