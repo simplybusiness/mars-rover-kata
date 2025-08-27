4e1203f62b939720052cf8c2fcfb47faf7fcfa13
Paul Cox <paul.cox@simplybusiness.co.uk>
2025-08-27 10:32:12 +0100

    more tests

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,3 +1,7 @@
 describe 'Mars Rover' do 
   it 'has an initial starting point'
+  it 'has a direction that it is facing (e.g. North)'
+  it 'has a direction that it is facing (e.g. East)'
+  it 'has a direction that it is facing (e.g. West)'
+  it 'has a direction that it is facing (e.g. South)'
 end
