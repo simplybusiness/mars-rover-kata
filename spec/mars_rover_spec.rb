cb8bfcc4dec7d3be7cc61fae9c75b6ffb9fe16d6
James Hurndall <James.hurndall@simplybusiness.co.uk>
2025-08-27 10:30:49 +0100

    added more directions to test

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,3 +1,7 @@
 describe 'mars rover' do
   it 'has an initial starting point'
+  it 'has a direction it is facing e.g. N'
+  it 'has a direction it is facing e.g. E'
+  it 'has a direction it is facing e.g. S'
+  it 'has a direction it is facing e.g. W'
 end
