790242fedd10a431b18a82138221d857e01d7312
Paul Cox <paul.cox@simplybusiness.co.uk>
2025-08-27 10:35:04 +0100

    more test

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -4,4 +4,7 @@ describe 'Mars Rover' do
   it 'has a direction that it is facing (e.g. East)'
   it 'has a direction that it is facing (e.g. West)'
   it 'has a direction that it is facing (e.g. South)'
+  it 'can not have a direction that is not, N, E, S, W'
+  it 'cannot have a nil starting point'
+  it 'cannot face multiple directions'
 end
