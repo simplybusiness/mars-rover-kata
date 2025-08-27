ede46724d414b037e3e531dcd396f23dbc6e4159
James Hurndall <James.hurndall@simplybusiness.co.uk>
2025-08-27 10:35:26 +0100

    added more tests

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -4,4 +4,7 @@ describe 'mars rover' do
   it 'has a direction it is facing e.g. E'
   it 'has a direction it is facing e.g. S'
   it 'has a direction it is facing e.g. W'
+  it 'cannot have a direction outside of N, E, S and W e.g R'
+  it 'cannot face N, E, S and W at the same time'
+  it 'cannot have a nil starting point'
 end
