a2a82c85ba18426b5c596d4ae767488a46780988
Alice Guyett <alice.guyett@simplybusiness.co.uk>
2025-08-27 10:35:10 +0100

    added tests for outliers

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -4,4 +4,7 @@ describe 'Mars Rover' do
 	it 'has a direction it is facing e.g. E'
 	it 'has a direction it is facing e.g. S'
 	it 'has a direction it is facing e.g. W'
+	it 'cannot have a direction outside of N, E, S, and W'
+	it 'cannot have a nil starting point'
+	it 'cannot face multiple directions'
 end
