57720f09da25fae622a1ef2a452a9e4ece0247bc
Alice Guyett <alice.guyett@simplybusiness.co.uk>
2025-08-27 10:30:38 +0100

    test - added tests for each direction

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,3 +1,7 @@
 describe 'Mars Rover' do
 	it 'has an initial starting point'
+	it 'has a direction it is facing e.g. N'
+	it 'has a direction it is facing e.g. E'
+	it 'has a direction it is facing e.g. S'
+	it 'has a direction it is facing e.g. W'
 end
