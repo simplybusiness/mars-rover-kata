7c998a7ce71bad43c8d3dc39e8220dd52448da14
James Hurndall <James.hurndall@simplybusiness.co.uk>
2025-08-27 10:51:40 +0100

    mars rover starts at the origin

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,5 +1,24 @@
-describe 'mars rover' do
-  it 'has an initial starting point'
+describe 'Mars Rover' do
+
+   class MarsRover
+
+    def initialize(starting_point:)
+
+    end
+
+    def current_position
+      [0, 0]
+    end
+
+   end
+
+
+  it 'has an initial starting point' do
+    mars_rover = MarsRover.new(starting_point: [0,0])
+  
+    expect(mars_rover.current_position).to eq([0,0])
+end
+
   it 'has a direction it is facing e.g. N'
   it 'has a direction it is facing e.g. E'
   it 'has a direction it is facing e.g. S'
