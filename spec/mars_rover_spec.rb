106c2d97deab3cfb1095a2fec4d2bde6c48120a4
Paul Cox <paul.cox@simplybusiness.co.uk>
2025-08-27 11:14:22 +0100

    tests

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,10 +1,21 @@
-describe 'Mars Rover' do 
-  it 'has an initial starting point'
-  it 'has a direction that it is facing (e.g. North)'
-  it 'has a direction that it is facing (e.g. East)'
-  it 'has a direction that it is facing (e.g. West)'
-  it 'has a direction that it is facing (e.g. South)'
-  it 'can not have a direction that is not, N, E, S, W'
+describe 'Mars Rover' do
+  class MarsRover
+      def initialize(starting_point:)
+  end
+  def current_position 
+      [0, 0]
+  end
+end
+
+  it 'has an initial starting point' do
+      mars_rover = MarsRover.new(starting_point: [0, 0])
+      expect(mars_rover.current_position).to eq([0, 0])
+  end
+  it 'has a direction it is facing e.g. N'
+  it 'has a direction it is facing e.g. E'
+  it 'has a direction it is facing e.g. S'
+  it 'has a direction it is facing e.g. W'
+  it 'cannot have a direction other than N, E, S and W e.g. R'
+  it 'cannot face N, E, S and W simultaneously'
   it 'cannot have a nil starting point'
-  it 'cannot face multiple directions'
 end
