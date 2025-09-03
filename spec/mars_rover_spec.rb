90a42a1e54251ec023fcc2f1679d16661096f3ef
Paul Cox <paul.cox@simplybusiness.co.uk>
2025-09-03 10:51:36 +0100

    week 2

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,20 +1,49 @@
 describe 'Mars Rover' do
   class MarsRover
-      def initialize(starting_point:)
+    attr_reader :current_position, :current_direction
+
+    def initialize(starting_point:, starting_direction:)
+      @current_position = starting_point
+      @current_direction = starting_direction
+    end
   end
-  def current_position 
-      [0, 0]
-  end
-end
 
   it 'has an initial starting point' do
-      mars_rover = MarsRover.new(starting_point: [0, 0])
-      expect(mars_rover.current_position).to eq([0, 0])
+    mars_rover = MarsRover.new(starting_point: [0, 0], starting_direction: 'N')
+
+    expect(mars_rover.current_position).to eq([0, 0])
   end
-  it 'has a direction it is facing e.g. N'
+
+  it 'has an initial starting point anywhere on Mars' do
+    mars_rover = MarsRover.new(starting_point: [1, 1], starting_direction: 'N')
+
+    expect(mars_rover.current_position).to eq([1, 1])
+  end
+
+  it 'has a direction it is facing in e.g. N' do
+    mars_rover = MarsRover.new(starting_point: [0, 0], starting_direction: 'N')
+
+    expect(mars_rover.current_direction).to eq 'N'
+  end
+
   it 'has a direction it is facing e.g. E'
+    mars_rover = MarsRover.new(starting_point: [1, 1], starting_direction: 'E') do
+
+    expect(mars_rover.current_direction).to eq 'E'
+  end
+
   it 'has a direction it is facing e.g. S'
+    mars_rover = MarsRover.new(starting_point: [1, 1], starting_direction: 'S') do
+
+    expect(mars_rover.current_direction).to eq 'S'
+  end
+
   it 'has a direction it is facing e.g. W'
+    mars_rover = MarsRover.new(starting_point: [1, 1], starting_direction: 'W') do
+
+    expect(mars_rover.current_direction).to eq 'W'
+  end
+
   it 'cannot have a direction other than N, E, S and W e.g. R'
   it 'cannot face N, E, S and W simultaneously'
   it 'cannot have a nil starting point'
