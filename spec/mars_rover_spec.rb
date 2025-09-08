a83283e323f47a45d5be49dd91e20ea7d8bb785b
James Hurndall <James.hurndall@simplybusiness.co.uk>
2025-09-08 15:01:12 +0100

    added homework test for only 4 directions allowed

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,9 +1,15 @@
 describe 'Mars Rover' do
 
    class MarsRover
+    VALID_DIRECTIONS = %w[N E S W].freeze
+
     attr_reader :current_position, :current_direction
 
     def initialize(starting_point:, starting_direction:)
+      unless VALID_DIRECTIONS.include?(starting_direction)
+      raise ArgumentError, "Starting direction must be one of #{VALID_DIRECTIONS.join(', ')}"
+    end
+
       @current_position  = starting_point
       @current_direction  = starting_direction
     end
@@ -43,7 +49,11 @@ describe 'Mars Rover' do
     expect(mars_rover.current_direction).to eq 'W'
   end
 
-  it 'cannot have a direction outside of N, E, S and W e.g R'
+  it 'cannot have a direction outside of N, E, S and W e.g R' do
+    expect { MarsRover.new(starting_point: [1, 3], starting_direction: 'X') }.to raise_error(ArgumentError)
+  end
+
+
   it 'cannot face N, E, S and W at the same time'
   it 'cannot have a nil starting point'
 end
