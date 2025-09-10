e56ca98dc7ac9a64f0c2f0b6ddb1a82197c33abc
Grace Calianese <grace.calianese@simplybusiness.com>
2025-09-10 12:12:27 -0400

    refactor coordinate check

--- mars_rover.rb
+++ mars_rover.rb
@@ -2,9 +2,7 @@ class MarsRover
     VALID_DIRECTIONS = ['N', 'E', 'S', 'W']
 
     def initialize(starting_position:, starting_direction:)
-        if starting_position.nil? || !starting_position.is_a?(Coordinates)
-          raise ArgumentError, "Starting position is invalid and must be valid coordinates"
-        end
+        raise ArgumentError, "Starting position is invalid and must be valid coordinates" if !starting_position.is_a?(Coordinates)
         if !VALID_DIRECTIONS.include?(starting_direction)
             raise ArgumentError, "Starting direction is invalid and must be N, E, S, or W"     
         end  
