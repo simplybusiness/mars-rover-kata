ea98be0c32dcb58df6bd9e663df77b5006bb4a5d
Grace Calianese <grace.calianese@simplybusiness.com>
2025-09-10 11:45:43 -0400

    use coordinates

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe 'mars rover' do
     end
 
     it 'has a current position away from the origin' do
-      mars_rover = MarsRover.new(starting_position: [0, 1], starting_direction: "N")
+      mars_rover = MarsRover.new(starting_position: Coordinates.new(0, 1), starting_direction: "N")
 
-      expect(mars_rover.current_position).to eq [0, 1]
+      expect(mars_rover.current_position).to eq Coordinates.new(0, 1)
     end
 
     it 'has a starting direction of north' do
-        mars_rover = MarsRover.new(starting_position: [0, 0], starting_direction: "N")
+        mars_rover = MarsRover.new(starting_position: Coordinates.new(0, 0), starting_direction: "N")
         expect(mars_rover.direction).to eq "N"
     end
 
@@ -23,7 +23,7 @@ RSpec.describe 'mars rover' do
     invalid_directions.each do |invalid_direction|
         it "cannot have an invalid direction e.g. #{invalid_direction.inspect}" do
             expect {
-                MarsRover.new(starting_position: [0, 0], starting_direction: invalid_direction)
+                MarsRover.new(starting_position: Coordinates.new(0, 0), starting_direction: invalid_direction)
             }.to raise_error(ArgumentError, "Starting direction is invalid and must be N, E, S, or W")
         end
     end
