7cb4eb5863315bbb89f57813b03f0550716d860a
william seward <william.seward@simplybusiness.com>
2025-09-12 10:02:38 -0400

    all working kata

---
+++ rover.rb
@@ -0,0 +1,79 @@
+class Rover
+    CARDINAL_DIRECTIONS = ['N', 'S', 'E', 'W']
+
+    attr_reader :x, :y, :direction, :commands
+
+    def initialize(x: 0, y: 0, direction: 'N', commands: [])
+
+        # coords are numbers
+        unless x.is_a?(Numeric) && y.is_a?(Numeric)
+            raise ArgumentError, 'Invalid coordinates, must be numbers'
+        end
+
+        # the direction is valid
+        unless CARDINAL_DIRECTIONS.include?(direction)
+            raise ArgumentError, "Invalid direction, must be one of #{CARDINAL_DIRECTIONS.join(', ')}"
+        end
+
+        # the commands are an array of characters
+        unless commands.is_a?(Array) && commands.all? { |c| c.is_a?(String) && c.length == 1 }
+            raise ArgumentError, 'Commands must be an array of characters'
+        end
+
+        @x = x
+        @y = y
+        @direction = direction
+        @commands = commands
+    end
+
+    def move
+        commands.each do |command|
+            case command.downcase
+            when 'f'
+                case @direction
+                when 'N'
+                    @y += 1
+                when 'S'
+                    @y -= 1
+                when 'E'
+                    @x += 1
+                when 'W'
+                    @x -= 1
+                end
+            when 'b'
+                case @direction
+                when 'N'
+                    @y -= 1
+                when 'S'
+                    @y += 1
+                when 'E'
+                    @x -= 1
+                when 'W'
+                    @x += 1
+                end
+            when 'l'
+                case @direction
+                when 'N'
+                    @x -= 1
+                when 'S'
+                    @x += 1
+                when 'E'
+                    @y += 1
+                when 'W'
+                    @y -= 1
+                end
+            when 'r'
+                case @direction
+                when 'N'
+                    @x += 1
+                when 'S'
+                    @x -= 1
+                when 'E'
+                    @y -= 1
+                when 'W'
+                    @y += 1
+                end
+            end
+        end
+    end
+end
---
+++ spec/rover_spec.rb
@@ -0,0 +1,208 @@
+require 'spec_helper'
+require_relative '../rover'
+
+RSpec.describe Rover do
+    it 'has a starting position that is not null' do
+        rov = Rover.new
+        expect(rov.x).not_to be_nil
+        expect(rov.y).not_to be_nil
+    end
+
+    it 'can be initialized with custom starting coordinates' do
+        rov = Rover.new(x: 3, y: 3)
+        expect(rov.x).to eq(3)
+        expect(rov.y).to eq(3)
+    end
+
+    it 'raises an argument error when the coordinates passed are not floats' do
+        expect { Rover.new(x: 'Z') }.to raise_error(ArgumentError, /Invalid coordinates/)
+        expect { Rover.new(y: 'Z') }.to raise_error(ArgumentError, /Invalid coordinates/)
+    end
+
+    it 'has a starting direction that is one of N, S, E, W' do
+        rov = Rover.new
+        valid_directions = ["N", "S", "E", "W"]
+        expect(valid_directions).to include(rov.direction)
+    end
+
+    it 'can be initialized with all custom directions' do
+        rov_s = Rover.new(direction: 'S')
+        expect(rov_s.direction).to eq('S')
+
+        rov_e = Rover.new(direction: 'E')
+        expect(rov_e.direction).to eq('E')
+
+        rov_w = Rover.new(direction: 'W')
+        expect(rov_w.direction).to eq('W')
+    end
+
+    it 'raises an argument error when initialized with an invalid direction' do
+        expect { Rover.new(direction: 'Z') }.to raise_error(ArgumentError, /Invalid direction/)
+    end
+
+    it 'holds the character array of commands' do
+        rov = Rover.new(commands: ['f', 'b', 'f', 'b'])
+        expect(rov.commands).to eq(['f', 'b', 'f', 'b'])
+
+        expect { Rover.new(commands: [1, 2, 3]) }.to raise_error(ArgumentError, /Commands must be/)
+        expect { Rover.new(commands: [1]) }.to raise_error(ArgumentError, /Commands must be/)
+        expect { Rover.new(commands: ["A", 23]) }.to raise_error(ArgumentError, /Commands must be/)
+        expect { Rover.new(commands: ['A', "ABSDF"]) }.to raise_error(ArgumentError, /Commands must be/)
+    end
+
+    it 'moves forward correctly' do
+        rov = Rover.new(commands: ['f', 'f', 'f'])
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(0)
+
+        rov.move()
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(3)
+
+        rov_s = Rover.new(direction: 'S', commands: ['f', 'f', 'f'])
+        expect(rov_s.x).to eq(0)
+        expect(rov_s.y).to eq(0)
+
+        rov_s.move()
+        expect(rov_s.x).to eq(0)
+        expect(rov_s.y).to eq(-3)
+
+        rov_e = Rover.new(direction: 'E', commands: ['f'])
+        expect(rov_e.x).to eq(0)
+        expect(rov_e.y).to eq(0)
+
+        rov_e.move()
+        expect(rov_e.x).to eq(1)
+        expect(rov_e.y).to eq(0)
+
+        rov_w = Rover.new(direction: 'W', commands: ['f', 'f'])
+        expect(rov_w.x).to eq(0)
+        expect(rov_w.y).to eq(0)
+
+        rov_w.move()
+        expect(rov_w.x).to eq(-2)
+        expect(rov_w.y).to eq(0)
+    end
+
+    it 'moves backward correctly' do
+        rov = Rover.new(commands: ['b', 'b', 'b'])
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(0)
+
+        rov.move()
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(-3)
+
+        rov_s = Rover.new(direction: 'S', commands: ['b', 'b', 'b'])
+        expect(rov_s.x).to eq(0)
+        expect(rov_s.y).to eq(0)
+
+        rov_s.move()
+        expect(rov_s.x).to eq(0)
+        expect(rov_s.y).to eq(3)
+
+        rov_e = Rover.new(direction: 'E', commands: ['b'])
+        expect(rov_e.x).to eq(0)
+        expect(rov_e.y).to eq(0)
+
+        rov_e.move()
+        expect(rov_e.x).to eq(-1)
+        expect(rov_e.y).to eq(0)
+
+        rov_w = Rover.new(direction: 'W', commands: ['b', 'b'])
+        expect(rov_w.x).to eq(0)
+        expect(rov_w.y).to eq(0)
+
+        rov_w.move()
+        expect(rov_w.x).to eq(2)
+        expect(rov_w.y).to eq(0)
+    end
+
+    it 'can move backwards and forwards in the same command list' do
+        rov = Rover.new(commands: ['f', 'f', 'b', 'b', 'b'])
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(0)
+
+        rov.move()
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(-1)
+    end
+
+    it 'moves left correctly' do
+        rov = Rover.new(commands: ['l', 'l', 'l'])
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(0)
+
+        rov.move()
+        expect(rov.x).to eq(-3)
+        expect(rov.y).to eq(0)
+
+        rov_s = Rover.new(direction: 'S', commands: ['l', 'l', 'l'])
+        expect(rov_s.x).to eq(0)
+        expect(rov_s.y).to eq(0)
+
+        rov_s.move()
+        expect(rov_s.x).to eq(3)
+        expect(rov_s.y).to eq(0)
+
+        rov_e = Rover.new(direction: 'E', commands: ['l'])
+        expect(rov_e.x).to eq(0)
+        expect(rov_e.y).to eq(0)
+
+        rov_e.move()
+        expect(rov_e.x).to eq(0)
+        expect(rov_e.y).to eq(1)
+
+        rov_w = Rover.new(direction: 'W', commands: ['l', 'l'])
+        expect(rov_w.x).to eq(0)
+        expect(rov_w.y).to eq(0)
+
+        rov_w.move()
+        expect(rov_w.x).to eq(0)
+        expect(rov_w.y).to eq(-2)
+    end
+
+    it 'moves right correctly' do
+        rov = Rover.new(commands: ['r', 'r', 'r'])
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(0)
+
+        rov.move()
+        expect(rov.x).to eq(3)
+        expect(rov.y).to eq(0)
+
+        rov_s = Rover.new(direction: 'S', commands: ['r', 'r', 'r'])
+        expect(rov_s.x).to eq(0)
+        expect(rov_s.y).to eq(0)
+
+        rov_s.move()
+        expect(rov_s.x).to eq(-3)
+        expect(rov_s.y).to eq(0)
+
+        rov_e = Rover.new(direction: 'E', commands: ['r'])
+        expect(rov_e.x).to eq(0)
+        expect(rov_e.y).to eq(0)
+
+        rov_e.move()
+        expect(rov_e.x).to eq(0)
+        expect(rov_e.y).to eq(-1)
+
+        rov_w = Rover.new(direction: 'W', commands: ['r', 'r'])
+        expect(rov_w.x).to eq(0)
+        expect(rov_w.y).to eq(0)
+
+        rov_w.move()
+        expect(rov_w.x).to eq(0)
+        expect(rov_w.y).to eq(2)
+    end
+
+    it 'can move all directions in the same command list' do
+        rov = Rover.new(commands: ['f', 'f', 'b', 'r', 'l', 'r'])
+        expect(rov.x).to eq(0)
+        expect(rov.y).to eq(0)
+
+        rov.move()
+        expect(rov.x).to eq(1)
+        expect(rov.y).to eq(1)
+    end
+end
