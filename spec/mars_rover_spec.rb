382733b8b0407814a83d669449c1f0e7267af878
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-09-19 14:01:15 +0100

    feat: a Mars Rover can have a starting position anywhere on Mars.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'Mars Rover' do
     attr_reader :current_position
 
     def initialize(starting_position:)
-      @current_position = [1, 1]
+      @current_position = starting_position
     end
   end
 
@@ -11,6 +11,12 @@ RSpec.describe 'Mars Rover' do
     mars_rover = MarsRover.new(starting_position: [1, 1])
 
     expect(mars_rover.current_position).to eq([1, 1])
+  end
+
+  it 'knows its position when it is located anywhere' do
+    mars_rover = MarsRover.new(starting_position: [0, 5])
+
+    expect(mars_rover.current_position).to eq([0, 5])
   end
 
   it 'knows the direction it is facing'
