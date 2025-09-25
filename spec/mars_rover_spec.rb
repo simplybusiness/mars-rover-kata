0bb47fd6b4165d032db92bd516ce80e61647acd8
David Dobbie <david.dobbie@simplybusiness.co.uk>
2025-09-25 14:43:42 +0100

    test: added test to fail if starting_position is missing

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -33,8 +33,9 @@ RSpec.describe 'Mars Rover' do
     expect(mars_rover.current_direction).to eq(direction)
   end
 
-  it 'complains that it does not have a starting point'
-
+  it 'complains that it does not have a starting point' do
+    expect{ MarsRover.new(starting_direction: 'N') }.to raise_error(StandardError)
+  end
   it 'complains that it does not have a direction' do
     expect{ MarsRover.new(starting_position: [1,1]) }.to raise_error(StandardError)
   end
