9ae50386d7ef107f583bc7a2f57df64fa6d58794
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-09-26 15:03:17 +0100

    refactor: removed the old thing.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -14,8 +14,6 @@ RSpec.describe 'Mars Rover' do
       @current_position = [@current_position[0], @current_position[1] + 1]
     end
 
-    alias_method :command, :execute
-
     def x_coordinate
       @current_position[0]
     end
