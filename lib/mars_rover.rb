54ed5d7004f2ecc1071166024a40e95f79c5a03b
Carlos Rubio <carlos.rubio@simplybusiness.co.uk>
2025-10-03 13:29:41 +0100

    removed the current_position implementation from moving forward.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -18,16 +18,12 @@ class MarsRover
   def move_forward
     case current_direction
     when 'N'
-      @current_position = [@current_position[0], @current_position[1] + 1]
       @current_coordinates = Coordinates.new(x: @current_coordinates.x, y: @current_coordinates.y + 1)
     when 'E'
-      @current_position = [@current_position[0] + 1, @current_position[1]]
       @current_coordinates = Coordinates.new(x: @current_coordinates.x + 1, y: @current_coordinates.y)
     when 'S'
-      @current_position = [@current_position[0], @current_position[1] - 1]
       @current_coordinates = Coordinates.new(x: @current_coordinates.x, y: @current_coordinates.y - 1)
     when 'W'
-      @current_position = [@current_position[0] - 1, @current_position[1]]
       @current_coordinates = Coordinates.new(x: @current_coordinates.x - 1, y: @current_coordinates.y)
     end
   end
