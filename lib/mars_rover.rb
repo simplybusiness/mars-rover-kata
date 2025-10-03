f80538e17095c7fe0d29872e867c3eb6f27031ca
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-10-03 13:23:10 +0100

    refactor: migrated north to also change current coordinates.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -22,6 +22,7 @@ class MarsRover
       @current_coordinates = Coordinates.new(x: @current_coordinates.x, y: @current_coordinates.y + 1)
     when 'E'
       @current_position = [@current_position[0] + 1, @current_position[1]]
+      @current_coordinates = Coordinates.new(x: @current_coordinates.x + 1, y: @current_coordinates.y)
     when 'S'
       @current_position = [@current_position[0], @current_position[1] - 1]
     when 'W'
