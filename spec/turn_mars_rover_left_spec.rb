58de3a63c02bc7c7ab79ed0b3c1608e94c5fd4e5
william seward <william.seward@simplybusiness.com>
2025-10-13 10:12:37 -0400

    using lambdas to test turning execution

--- spec/turn_mars_rover_left_spec.rb
+++ spec/turn_mars_rover_left_spec.rb
@@ -3,41 +3,31 @@ require_relative '../rover'
 RSpec.describe Rover do
   it 'faces west after turning left when facing north' do
     rov = Rover.new(direction: 'N', commands: ['l'])
-    expect(rov.direction).to eq('N')
 
-    rov.move
-    expect(rov.direction).to eq('W')
+    expect{rov.move}.to change(rov, :direction).from('N').to('W')
   end
 
   it 'faces north after turning left when facing east' do
     rov = Rover.new(direction: 'E', commands: ['l'])
-    expect(rov.direction).to eq('E')
 
-    rov.move
-    expect(rov.direction).to eq('N')
+    expect{rov.move}.to change(rov, :direction).from('E').to('N')
   end
 
   it 'faces east after turning left when facing south' do
     rov = Rover.new(direction: 'S', commands: ['l'])
-    expect(rov.direction).to eq('S')
 
-    rov.move
-    expect(rov.direction).to eq('E')
+    expect{rov.move}.to change(rov, :direction).from('S').to('E')
   end
 
   it 'faces south after turning left when facing west' do
     rov = Rover.new(direction: 'W', commands: ['l'])
-    expect(rov.direction).to eq('W')
 
-    rov.move
-    expect(rov.direction).to eq('S')
+    expect{rov.move}.to change(rov, :direction).from('W').to('S')
   end
 
   it 'faces the same direction when turning left 4 times' do
     rov = Rover.new(direction: 'N', commands: %w[l l l l])
-    expect(rov.direction).to eq('N')
 
-    rov.move
-    expect(rov.direction).to eq('N')
+    expect{rov.move}.to_not change(rov, :direction)
   end
 end
