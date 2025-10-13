e21b42f2f67516c7f070a3515a8fa0eefaeec49f
william seward <william.seward@simplybusiness.com>
2025-10-13 10:00:05 -0400

    refactor turn right tests

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -57,44 +57,4 @@ RSpec.describe Rover do
     rov.move
     expect(rov.current_position).to eq([0, -1])
   end
-
-  it 'faces east after turning right when facing north' do
-    rov = Rover.new(direction: 'N', commands: ['r'])
-    expect(rov.direction).to eq('N')
-
-    rov.move
-    expect(rov.direction).to eq('E')
-  end
-
-  it 'faces south after turning right when facing east' do
-    rov = Rover.new(direction: 'E', commands: ['r'])
-    expect(rov.direction).to eq('E')
-
-    rov.move
-    expect(rov.direction).to eq('S')
-  end
-
-  it 'faces west after turning right when facing south' do
-    rov = Rover.new(direction: 'S', commands: ['r'])
-    expect(rov.direction).to eq('S')
-
-    rov.move
-    expect(rov.direction).to eq('W')
-  end
-
-  it 'faces north after turning right when facing west' do
-    rov = Rover.new(direction: 'W', commands: ['r'])
-    expect(rov.direction).to eq('W')
-
-    rov.move
-    expect(rov.direction).to eq('N')
-  end
-
-  it 'faces the same direction when turning right 4 times' do
-    rov = Rover.new(direction: 'N', commands: %w[r r r r])
-    expect(rov.direction).to eq('N')
-
-    rov.move
-    expect(rov.direction).to eq('N')
-  end
 end
