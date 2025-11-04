35830c5685205ac4619fc9eff3fac611c1090066
Alice Guyett <alice.guyett@simplybusiness.co.uk>
2025-11-04 13:46:29 +0000

    added examples of moving forwards for all directions

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,39 +1,79 @@
 describe 'Mars Rover' do
   class MarsRover
-    attr_reader :current_position
+    attr_reader :current_position, :current_direction, :commands
 
-    def initialize(starting_point:)
+    def initialize(starting_point:, starting_direction:, commands: [])
+      raise ArgumentError, 'The starting direction must be one of N, E, S or W' unless ['N', 'E', 'S', 'W'].include? starting_direction
+      raise ArgumentError, 'The starting point cannot be nil' if starting_point.nil?
       @current_position = starting_point
-      @current_direction = 'N'
-    end
-
-    def current_direction
-    	@current_direction
+      @current_direction = starting_direction
+      @commands = commands
     end
   end
 
   it 'has an initial starting point' do
-    mars_rover = MarsRover.new(starting_point: [0, 0])
+    mars_rover = MarsRover.new(starting_point: [0, 0], starting_direction: 'N')
 
     expect(mars_rover.current_position).to eq([0, 0])
   end
 
   it 'has an initial starting point anywhere on Mars' do
-  	mars_rover = MarsRover.new(starting_point: [1, 1])
+    mars_rover = MarsRover.new(starting_point: [1, 1], starting_direction: 'N')
 
-    expect(mars_rover.current_position).to eq([1, 1])
+    expect(mars_rover.current_position).to eq [1, 1]
   end
 
   it 'has a direction it is facing e.g. N' do
-  	mars_rover = MarsRover.new(starting_point: [0, 0])
+    mars_rover = MarsRover.new(starting_point: [0, 0], starting_direction: 'N')
 
-  	expect(mars_rover.current_direction). to eq 'N'
+    expect(mars_rover.current_direction).to eq 'N'
   end
 
-  it 'has a direction it is facing e.g. E'
-  it 'has a direction it is facing e.g. S'
-  it 'has a direction it is facing e.g. W'
-  it 'cannot have a direction outside of N, E, S and W e.g. R'
-  it 'cannot face N, E, S and W simultaneously'
-  it 'cannot have a nil starting point'
+  it 'has a direction it is facing e.g. E' do
+    mars_rover = MarsRover.new(starting_point: [1, 1], starting_direction: 'E')
+
+    expect(mars_rover.current_direction).to eq 'E'
+  end
+
+  it 'has a direction it is facing e.g. S' do
+    mars_rover = MarsRover.new(starting_point: [1, 1], starting_direction: 'S')
+
+    expect(mars_rover.current_direction).to eq 'S'
+  end
+
+  it 'has a direction it is facing e.g. W' do
+    mars_rover = MarsRover.new(starting_point: [3, 4], starting_direction: 'W')
+
+    expect(mars_rover.current_direction).to eq 'W'
+  end
+
+  it 'cannot have a direction outside of N, E, S and W e.g. R' do
+    expect do
+      MarsRover.new(starting_point: [0, 1], starting_direction: 'C')
+    end.to raise_error(ArgumentError, 'The starting direction must be one of N, E, S or W')
+  end
+
+  it 'cannot face N, E, S and W simultaneously' do
+    expect { MarsRover.new(starting_point: [-3, 1], starting_direction: ['N', 'E', 'S', 'W']) }.to(
+      raise_error(ArgumentError, 'The starting direction must be one of N, E, S or W')
+    )
+  end
+  it 'cannot have a nil starting point' do
+    expect { MarsRover.new(starting_point: nil, starting_direction: 'E') }.to(
+      raise_error(ArgumentError, 'The starting point cannot be nil')
+    )
+  end
+
+  it 'receives a character array of commands from Earth' do
+    mars_rover = MarsRover.new(starting_point: [0, 1], starting_direction: 'E', commands: ['f', 'b'])
+
+    expect(mars_rover.commands).to eq ['f', 'b']
+  end
+
+  describe 'Moving forwards' do
+    example 'moving forwards when facing north'
+    example 'moving forwards when facing south'
+    example 'moving forwards when facing east'
+    example 'moving forwards when facing west'
+
 end
