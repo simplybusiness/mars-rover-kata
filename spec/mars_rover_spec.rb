b28fba32c38f86d56982de22aa4c016b94c00046
Marcus.Long <marcus.long@simplybusiness.co.uk>
2025-11-17 14:10:24 +0000

    refactor: removed obsolete test.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -87,14 +87,6 @@ describe 'Mars Rover' do
       end
     end
 
-    example 'moving forward when facing north from [0, 1]' do
-      mars_rover = MarsRover.new(starting_point: [0, 1], starting_direction: 'N', commands: ['f'])
-
-      mars_rover.execute
-
-      expect(mars_rover.current_position).to eq [0, 2]
-    end
-
     example 'moving forward when facing north from any position' do
       mars_rover = MarsRover.new(starting_point: [1, 2], starting_direction: 'N', commands: ['f'])
 
