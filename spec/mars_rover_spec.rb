3a2094865d3a1f3632c064fb9f6cc452d5ff6662
Alice Guyett <alice.guyett@simplybusiness.co.uk>
2025-11-25 16:19:29 +0000

    added test for south

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -11,7 +11,19 @@ describe 'Mars Rover' do
     end
 
     def execute
-      @current_position = [@current_position[0], @current_position[1] + 1]
+      if @current_direction == 'N'
+        @current_position = [@current_position[0], @current_position[1] + 1]
+      elsif @current_direction == 'S'
+        @current_position = [@current_position[0], @current_position[1] - 1]
+      end
+    end
+
+    def x_coordinate
+      @current_position[0]
+    end
+
+    def y_coordinate
+      @current_position[1]
     end
   end
 
@@ -75,21 +87,24 @@ describe 'Mars Rover' do
   end
 
   describe 'Moving forwards' do
-    [
-      [[0, 0], [0, 1]],
-      [[0, 1], [0, 2]],
-      [[1, 2], [1, 3]]
-    ].each do |initial_position, final_position|
-      example "moving forward when facing north e.g. from #{initial_position} to #{final_position}" do
-        mars_rover = MarsRover.new(starting_point: initial_position, starting_direction: 'N', commands: ['f'])
+    example "moving forward (1 step up the y-axis) when facing north" do
+      mars_rover = MarsRover.new(starting_point: [1, 2], starting_direction: 'N', commands: ['f'])
 
-        mars_rover.execute
+      mars_rover.execute
 
-        expect(mars_rover).to be_located_at final_position
-      end
+      expect { mars_rover.execute }.to change { mars_rover.y_coordinate }.by 1
+      expect { mars_rover.execute }.not_to change { mars_rover.x_coordinate }
     end
 
-    example 'moving forward when facing south'
+    example "moving forward (1 step down the y-axis) when facing south" do
+      mars_rover = MarsRover.new(starting_point: [1, 2], starting_direction: 'S', commands: ['f'])
+
+      mars_rover.execute
+
+      expect { mars_rover.execute }.to change { mars_rover.y_coordinate }.by(-1)
+      expect { mars_rover.execute }.not_to change { mars_rover.x_coordinate }
+    end
+
     example 'moving forward when facing east'
     example 'moving forward when facing west'
 
