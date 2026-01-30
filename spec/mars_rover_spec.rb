dff9b39d1f0b9b5acd448c1a5aaf042f4447d946
Omar Akmal <omar.akmal@simplybusiness.com>
2026-01-30 13:35:00 -0500

    style: formatted test

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe 'operating a mars rover' do
     expect(rover.direction).to eq(:north)
   end
   it "has a position on planet" do
-
     rover = MarsRover.new(1, 2)
+
     expect(rover.x_coordinate).to eq(1)
     expect(rover.y_coordinate).to eq(2)
   end
