07fe749d368a380455e0ba429d7976dfa15d3e03
Omar Akmal <omar.akmal@simplybusiness.com>
2026-01-30 13:31:16 -0500

    test: added a new test for the mars rover positioning

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -10,7 +10,12 @@ RSpec.describe 'operating a mars rover' do
 
     expect(rover.direction).to eq(:north)
   end
-  it "has a position on planet"
+  it "has a position on planet" do
+
+    rover = MarsRover.new(1, 2)
+    expect(rover.x_coordinate).to eq(1)
+    expect(rover.y_coordinate).to eq(2)
+  end
   it "has a valid direction"
 
 end
