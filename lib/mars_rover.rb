b6521ab2ad698e1ccae09191e839add8bc39775f
lyndbergh-simelus-sb <lyndbergh.simelus@simplybusiness.com>
2026-02-13 13:54:52 -0500

    delete default direction in init and fix test

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -2,7 +2,7 @@ class MarsRover
 
   attr_reader :x_coordinate, :y_coordinate, :direction
 
-  def initialize(x_coordinate, y_coordinate, direction: nil)
+  def initialize(x_coordinate, y_coordinate, direction: )
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
     @direction = direction
--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'operating a mars rover' do
     expect(rover.direction).to eq(:north)
   end
   it "has a position on planet" do
-    rover = MarsRover.new(1, 2)
+    rover = MarsRover.new(1, 2, direction: :north)
 
     expect(rover.x_coordinate).to eq(1)
     expect(rover.y_coordinate).to eq(2)
