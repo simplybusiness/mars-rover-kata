6bd023620ed6e6fd49bae5a17da031c4c832aebf
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2026-03-25 10:43:51 +0000

    feat: Compass turns right

--- lib/compass.rb
+++ lib/compass.rb
@@ -1,7 +1,14 @@
 class Compass
   attr_reader :direction
 
+  DIRECTIONS = %i[N E S W].freeze
+
   def initialize(direction)
     @direction = direction
+  end
+
+  def turn_right
+    current_index = DIRECTIONS.index(@direction)
+    Compass.new(DIRECTIONS[(current_index + 1) % 4])
   end
 end
--- spec/compass_spec.rb
+++ spec/compass_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Compass do
 
     expect(compass.direction).to eq(:N)
   end
+
+  it 'turns right from north to east' do
+    compass = Compass.new(:N)
+
+    expect(compass.turn_right.direction).to eq(:E)
+  end
 end
