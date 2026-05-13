ec9dcb15ff5741420130aeb302968a96540f785c
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2026-05-13 10:38:18 +0100

    remove attr width and height

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -3,13 +3,6 @@
 require_relative '../lib/grid'
 
 RSpec.describe Grid do
-  it 'has a width and height' do
-    grid = Grid.new(width: 10, height: 10)
-
-    expect(grid.width).to eq(10)
-    expect(grid.height).to eq(10)
-  end
-
   it 'wraps x-coordinate when exceeding width' do
     grid = Grid.new(width: 5, height: 5)
 
